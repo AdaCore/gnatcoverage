4c3582cbc02809d45aa756648580afbb1e4e0c29
Thomas Quinot <quinot@adacore.com>
2009-02-13 13:26:27 +0000

    Minor reformatting
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@1863 020d506d-db78-4e55-b2a8-6c851f84c332

--- tools/xcov/disa_ppc.adb
+++ tools/xcov/disa_ppc.adb
@@ -23,25 +23,52 @@ with Ppc_Descs; use Ppc_Descs;
 with Ppc_Disopc; use Ppc_Disopc;
 
 package body Disa_Ppc is
-   function Get_Insn_Length (Addr : System.Address) return Positive
-   is
+
+   ---------------------
+   -- Get_Insn_Length --
+   ---------------------
+
+   function Get_Insn_Length (Addr : System.Address) return Positive is
       pragma Unreferenced (Addr);
    begin
       return 4;
    end Get_Insn_Length;
 
-   procedure Disassemble_Insn (Addr : System.Address;
-                               Pc : Traces.Pc_Type;
-                               Line : out String;
-                               Line_Pos : out Natural;
-                               Insn_Len : out Natural;
-                               Sym : Symbolizer'Class)
+   ----------------------
+   -- Disassemble_Insn --
+   ----------------------
+
+   procedure Disassemble_Insn
+     (Addr     : System.Address;
+      Pc       : Traces.Pc_Type;
+      Line     : out String;
+      Line_Pos : out Natural;
+      Insn_Len : out Natural;
+      Sym      : Symbolizer'Class)
    is
       W : Unsigned_32;
 
-      --  Add CHAR to the line.
       procedure Add (C : Character);
       pragma Inline (Add);
+      --  Add C to the line
+
+      procedure Add (Str : String);
+      --  Add Str to the line
+
+      procedure Add_HT;
+      --  Comment needed???
+
+      subtype Natural_0_9 is Natural range 0 .. 9;
+
+      procedure Add_Digit (N : Natural_0_9);
+      procedure Add_Num2 (Reg : Unsigned_32);
+      procedure Add_Simm (Val : Unsigned_32);
+      procedure Add_Uimm (Val : Unsigned_32);
+      --  Comment needed???
+
+      ---------
+      -- Add --
+      ---------
 
       procedure Add (C : Character) is
       begin
@@ -51,7 +78,10 @@ package body Disa_Ppc is
          end if;
       end Add;
 
-      --  Add STR to the line.
+      ---------
+      -- Add --
+      ---------
+
       procedure Add (Str : String) is
       begin
          if Line_Pos + Str'Length <= Line'Last then
@@ -64,6 +94,10 @@ package body Disa_Ppc is
          end if;
       end Add;
 
+      ------------
+      -- Add_HT --
+      ------------
+
       procedure Add_HT is
       begin
          Add (' ');
@@ -72,14 +106,20 @@ package body Disa_Ppc is
          end loop;
       end Add_HT;
 
-      subtype Natural_0_9 is Natural range 0 .. 9;
+      ---------------
+      -- Add_Digit --
+      ---------------
+
       procedure Add_Digit (N : Natural_0_9) is
       begin
          Add (Character'Val (Character'Pos ('0') + N));
       end Add_Digit;
 
-      procedure Add_Num2 (Reg : Unsigned_32)
-      is
+      --------------
+      -- Add_Num2 --
+      --------------
+
+      procedure Add_Num2 (Reg : Unsigned_32) is
          H, L : Unsigned_32;
       begin
          L := Reg mod 10;
@@ -90,8 +130,11 @@ package body Disa_Ppc is
          Add_Digit (Natural (L));
       end Add_Num2;
 
-      procedure Add_Simm (Val : Unsigned_32)
-      is
+      --------------
+      -- Add_Simm --
+      --------------
+
+      procedure Add_Simm (Val : Unsigned_32) is
          V : constant Unsigned_16 := Unsigned_16 (Val);
       begin
          if (V and 16#8000#) /= 0 then
@@ -103,8 +146,11 @@ package body Disa_Ppc is
          end if;
       end Add_Simm;
 
-      procedure Add_Uimm (Val : Unsigned_32)
-      is
+      --------------
+      -- Add_Uimm --
+      --------------
+
+      procedure Add_Uimm (Val : Unsigned_32) is
          V : constant Unsigned_16 := Unsigned_16 (Val);
       begin
          Add ("0x");
@@ -113,15 +159,21 @@ package body Disa_Ppc is
 
       Insn_Index : Integer := -1;
       Opc : Natural;
+
+   --  Start of processing for Disassemble_Insn
+
    begin
       W := Get_Insn (Addr);
       Insn_Len := 4;
       Line_Pos := Line'First;
 
-      --  Find insn.
+      --  Find insn
+
       Opc := Natural (Get_Field (F_OPC, W));
-      --  Should be Ppc_Opc_Index (opc + 1) - 1, but this would miss
+
+      --  Should be Ppc_Opc_Index (Opc + 1) - 1, but this would miss
       --  the with-update instructions.
+
       for I in Ppc_Opc_Index (Opc) .. Ppc_Opc_Index (Opc + 1) loop
          if (W and Ppc_Insns (I).Mask) = Ppc_Insns (I).Insn then
             Insn_Index := I;
@@ -131,20 +183,24 @@ package body Disa_Ppc is
 
       if Insn_Index >= 0 then
          declare
-            Insn : Ppc_Insn_Descr renames Ppc_Insns (Insn_Index);
-            Has_Ht : Boolean := False;
+            Insn     : Ppc_Insn_Descr renames Ppc_Insns (Insn_Index);
+            Has_Ht   : Boolean := False;
             Is_First : Boolean := True;
-            F : Ppc_Fields;
-            Val : Unsigned_32;
+            F        : Ppc_Fields;
+            Val      : Unsigned_32;
+            --  Comments needed???
+
          begin
             --  Display mnemonic.
             --  This is a bounded string padded with spaces.
+
             for I in Insn.Name'Range loop
                exit when Insn.Name (I) = ' ';
                Add (Insn.Name (I));
             end loop;
 
-            --  Display fields.
+            --  Display fields
+
             for I in Insn.Fields'Range loop
                F := Insn.Fields (I);
                exit when F = F_Eof;
@@ -152,64 +208,80 @@ package body Disa_Ppc is
                   --  Some fields are parts of the mnemonic.
                   --  Add spaces when displaying the first field which is not
                   --  part of the mnemonic.
+
                   if not Has_Ht then
                      Add_HT;
                      Has_Ht := True;
                      Is_First := False;
+
                   elsif Is_First then
                      Is_First := False;
+
                   else
                      --  Add a comma separator between fields.
+
                      Add (',');
                   end if;
+
                else
                   pragma Assert (Is_First);
                   null;
                end if;
 
-               --  Extract field value.
+               --  Extract field value
+
                Val := Get_Field (F, W);
 
-               --  Display field value.
+               --  Display field value
+
                case F is
                   when F_OE =>
                      if Val /= 0 then
                         Add ('o');
                      end if;
+
                   when F_Rc =>
                      if Val /= 0 then
                         Add ('.');
                      end if;
+
                   when F_AA =>
                      if Val /= 0 then
                         Add ('a');
                      end if;
+
                   when F_LK =>
                      if Val /= 0 then
                         Add ('l');
                      end if;
+
                   when F_Br_Hint =>
                      if Val = 0 then
                         Add ('-');
                      else
                         Add ('+');
                      end if;
+
                   when F_L =>
                      if Val = 0 then
                         Add ('w');
                      else
                         Add ('d');
                      end if;
+
                   when F_U =>
                      if Val /= 0 then
                         Add ('u');
                      end if;
+
                   when F_A | F_D | F_S | F_B =>
                      Add ('r');
                      Add_Num2 (Val);
+
                   when F_FA | F_FD | F_FS | F_FB | F_FC =>
                      Add ('f');
                      Add_Num2 (Val);
+
                   when F_Disp =>
                      pragma Assert (Insn.Fields (I + 1) = F_A);
                      pragma Assert (Insn.Fields (I + 2) = F_Eof);
@@ -218,58 +290,80 @@ package body Disa_Ppc is
                      Add_Num2 (Get_Field (F_A, W));
                      Add (')');
                      exit;
+
                   when F_SIMM =>
                      Add_Simm (Val);
+
                   when F_UIMM =>
                      Add_Uimm (Val);
+
                   when F_LI =>
                      Val := Shift_Left (Val, 2);
+
                      --  Sign extend
+
                      if (Val and 16#800000#) /= 0 then
                         Val := Val or 16#Ff000000#;
                      end if;
-                     --  Test AA field.
+
+                     --  Test AA field
+
                      if (W and 2) = 0 then
                         Val := Val + Pc;
                      end if;
+
                      Add ("0x");
                      Add (Hex_Image (Val));
                      Sym.Symbolize (Val, Line, Line_Pos);
+
                   when F_BD =>
                      Val := Shift_Left (Val, 2);
+
                      --  Sign extend
+
                      if (Val and 16#8000#) /= 0 then
                         Val := Val or 16#Ffff0000#;
                      end if;
-                     --  Test AA field.
+
+                     --  Test AA field
+
                      if (W and 2) = 0 then
                         Val := Val + Pc;
                      end if;
                      Add ("0x");
                      Add (Hex_Image (Val));
                      Sym.Symbolize (Val, Line, Line_Pos);
+
                   when F_CrfD | F_CrfS =>
                      Add ("cr");
                      Add_Num2 (Val);
+
                   when F_BI | F_BO | F_SH | F_MB | F_ME =>
                      Add_Num2 (Val);
+
                   when others =>
                      Add (Hex_Image (Val));
                end case;
             end loop;
          end;
+
       else
          Add (".long ");
          Add (Hex_Image (W));
       end if;
    end Disassemble_Insn;
 
-   procedure Get_Insn_Properties (Insn : Unsigned_32;
-                                  Pc : Unsigned_32;
-                                  Branch : out Branch_Kind;
-                                  Flag_Indir : out Boolean;
-                                  Flag_Cond : out Boolean;
-                                  Dest : out Unsigned_32)
+   -------------------------
+   -- Get_Insn_Properties --
+   -------------------------
+
+   procedure Get_Insn_Properties
+     (Insn       : Unsigned_32;
+      Pc         : Unsigned_32;
+      Branch     : out Branch_Kind;
+      Flag_Indir : out Boolean;
+      Flag_Cond  : out Boolean;
+      Dest       : out Unsigned_32)
    is
       Opc, Xo, Bo : Unsigned_32;
       D : Unsigned_32;
@@ -277,7 +371,8 @@ package body Disa_Ppc is
       Opc := Get_Field (F_OPC, Insn);
       Xo := Get_Field (F_XO, Insn);
 
-      --  To be overriden for non-common cases.
+      --  To be overriden for non-common cases
+
       if Get_Field (F_LK, Insn) = 1 then
          Branch := Br_Call;
       else
@@ -286,6 +381,7 @@ package body Disa_Ppc is
 
       if Opc = 18 then
          --  Opc = 18: b, ba, bl and bla
+
          Flag_Indir := False;
          Flag_Cond := False;
          D := Shift_Left (Get_Signed_Field (F_LI, Insn), 2);
@@ -295,8 +391,10 @@ package body Disa_Ppc is
             Dest := Pc + D;
          end if;
          return;
+
       elsif Opc = 16 then
-         --  bcx.
+         --  bcx
+
          Flag_Indir := False;
          D := Shift_Left (Get_Signed_Field (F_BD, Insn), 2);
          if Get_Field (F_AA, Insn) = 1 then
@@ -304,17 +402,22 @@ package body Disa_Ppc is
          else
             Dest := Pc + D;
          end if;
+
       elsif Opc = 19 and Xo = 16 then
-         --  bclrx.
+         --  bclrx
+
          Flag_Indir := True;
          Dest := 0;
          if Branch = Br_Jmp then
             Branch := Br_Ret;
          end if;
+
       elsif Opc = 19 and Xo = 528 then
          --  bcctrx
+
          Flag_Indir := True;
          Dest := 0;
+
       else
          Branch := Br_None;
          Flag_Indir := False;
@@ -326,4 +429,5 @@ package body Disa_Ppc is
       Bo := Get_Field (F_BO, Insn);
       Flag_Cond := not ((Bo and 2#10100#) = 2#10100#);
    end Get_Insn_Properties;
+
 end Disa_Ppc;
--- tools/xcov/disa_ppc.ads
+++ tools/xcov/disa_ppc.ads
@@ -16,30 +16,36 @@
 -- Boston, MA 02111-1307, USA.                                              --
 --                                                                          --
 ------------------------------------------------------------------------------
+
 with System;
 with Interfaces;
-with Traces; use Traces;
+with Traces;         use Traces;
 with Disa_Symbolize; use Disa_Symbolize;
 
 package Disa_Ppc is
-   --  Return the length of the instruction at Addr.
+
    function Get_Insn_Length (Addr : System.Address) return Positive;
    pragma Inline (Get_Insn_Length);
+   --  Return the length of the instruction at Addr
 
+   procedure Disassemble_Insn
+     (Addr     : System.Address;
+      Pc       : Pc_Type;
+      Line     : out String;
+      Line_Pos : out Natural;
+      Insn_Len : out Natural;
+      Sym      : Symbolizer'Class);
    --  Disassemble instruction at ADDR, and put the result in LINE/LINE_POS.
    --  LINE_POS is the index of the next character to be written (ie line
-   --   length if Line'First = 1).
-   procedure Disassemble_Insn (Addr : System.Address;
-                               Pc : Pc_Type;
-                               Line : out String;
-                               Line_Pos : out Natural;
-                               Insn_Len : out Natural;
-                               Sym : Symbolizer'Class);
+   --  length if Line'First = 1).
 
-   procedure Get_Insn_Properties (Insn : Interfaces.Unsigned_32;
-                                  Pc : Pc_Type;
-                                  Branch : out Branch_Kind;
-                                  Flag_Indir : out Boolean;
-                                  Flag_Cond : out Boolean;
-                                  Dest : out Pc_Type);
+   procedure Get_Insn_Properties
+     (Insn       : Interfaces.Unsigned_32;
+      Pc         : Pc_Type;
+      Branch     : out Branch_Kind;
+      Flag_Indir : out Boolean;
+      Flag_Cond  : out Boolean;
+      Dest       : out Pc_Type);
+   --  Comment needed???
+
 end Disa_Ppc;
