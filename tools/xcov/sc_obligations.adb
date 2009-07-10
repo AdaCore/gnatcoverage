656cefe9900144fa74180db2b50fc8bb885ad77a
Thomas Quinot <quinot@adacore.com>
2009-07-10 15:40:44 +0000

    (Load_SCOs_From_ALI): Build a BDD for each decision. For I223-027
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2114 020d506d-db78-4e55-b2a8-6c851f84c332

--- tools/xcov/sc_obligations.adb
+++ tools/xcov/sc_obligations.adb
@@ -46,6 +46,22 @@ package body SC_Obligations is
    use type Pc_Type;
    package PC_Sets is new Ada.Containers.Ordered_Sets (Pc_Type);
 
+   --  Types used to include BDD information in SCO descriptors for decisions
+
+   type Tristate is (False, True, Unknown);
+
+   type Condition_Outcome is record
+      Decision_Outcome : Tristate := Unknown;
+      --  For a condition outcome that determines the final decision outcome,
+      --  indicates of the corresponding decision outcome value: False or True.
+      --  For a condition outcome that chains to a subsequent condition,
+      --  Unknown.
+
+      Next_SCO : SCO_Id := No_SCO_Id;
+      --  Reference to the Decision if Decision_Outcome is False or True.
+      --  Reference to the next condition to be tested if Unknown.
+   end record;
+
    type SCO_Descriptor (Kind : SCO_Kind := SCO_Kind'First) is record
       First_Sloc : Source_Location;
       --  First sloc (for a complex decision, taken from first condition)
@@ -61,14 +77,28 @@ package body SC_Obligations is
 
       case Kind is
          when Condition =>
+            Value : Tristate;
+            --  Indicates whether this condition is always true, always false,
+            --  or tested at run time (Unknown).
+
             PC_Set : PC_Sets.Set;
             --  Addresses of conditional branches testing this condition
+            --  (if Value = Unknown).
+
+            False_Outcome, True_Outcome : Condition_Outcome;
+            --  Condition outcomes (i.e. outgoing arcs from this condition in
+            --  the BDD) for the False and True cases.
 
          when Decision =>
             Is_Complex_Decision : Boolean;
             --  True for complex decisions.
             --  Note that there is always a distinct Condition SCO descriptor,
             --  even for simple decisions.
+
+            First_Condition : SCO_Id := No_SCO_Id;
+            --  Leftmost condition in decision. Note that the first tested
+            --  decision is the first *non-constant* (Value = Unknown)
+            --  condition in the decision.
          when others =>
             null;
       end case;
@@ -81,6 +111,436 @@ package body SC_Obligations is
        (Index_Type   => Valid_SCO_Id,
         Element_Type => SCO_Descriptor);
    SCO_Vector : SCO_Vectors.Vector;
+
+   --  Supporting functionality to build binary decision diagrams
+
+   package BDD is
+      --  Outgoing arcs from a BDD node
+
+      type BDD_Node_Id is new Nat;
+      No_BDD_Node_Id : constant BDD_Node_Id := 0;
+      subtype Valid_BDD_Node_Id is BDD_Node_Id
+      range No_BDD_Node_Id + 1 .. BDD_Node_Id'Last;
+
+      type Destinations is record
+         Dest_False, Dest_True : BDD_Node_Id;
+      end record;
+
+      --  BDD node kinds
+
+      type BDD_Node_Kind is
+        (Exit_False,
+         --  Leaf (decision outcome is False)
+
+         Exit_True,
+         --  Leaf (decision outcome is True),
+
+         Condition,
+         --  Evaluate condition
+
+         Jump);
+         --  Indirect reference to another BDD node
+
+      type BDD_Node (Kind : BDD_Node_Kind := Exit_False) is record
+         case Kind is
+            when Exit_False | Exit_True =>
+               null;
+
+            when Condition =>
+               C_SCO : SCO_Id;
+               --  Condition SCO
+
+               Dests : Destinations;
+               --  Outgoing arcs depending on this condition
+
+            when Jump =>
+               Dest : BDD_Node_Id := No_BDD_Node_Id;
+               --   Next BDD node
+         end case;
+      end record;
+
+      package BDD_Vectors is
+        new Ada.Containers.Vectors
+          (Index_Type   => Valid_BDD_Node_Id,
+           Element_Type => BDD_Node);
+
+      type BDD_Type is record
+         Decision       : SCO_Id;
+         Root_Condition : BDD_Node_Id := No_BDD_Node_Id;
+         V              : BDD_Vectors.Vector;
+      end record;
+
+      procedure Allocate
+        (BDD     : in out BDD_Type;
+         Node    : BDD_Node;
+         Node_Id : out BDD_Node_Id);
+      --  Allocate a node within the given BDD with the given properties
+
+      --  Arcs descriptor used while building a BDD
+
+      type Arcs is record
+         Dests  : Destinations;
+         --  Outgoing arcs for next condition
+
+         Origin : BDD_Node_Id := No_BDD_Node_Id;
+         --  Jump node referencing next condition
+      end record;
+
+      procedure Push (A : Arcs);
+      function Pop return Arcs;
+      --  Manage a stack of Arcs
+
+      --  Construction of a BDD
+
+      function Create (Decision : SCO_Id) return BDD_Type;
+      --  Start construction of a new BDD for the given decision
+
+      procedure Process_Not      (BDD : BDD_Type);
+      procedure Process_And_Then (BDD : in out BDD_Type);
+      procedure Process_Or_Else  (BDD : in out BDD_Type);
+      --  Process NOT, AND THEN, OR ELSE operators
+
+      procedure Process_Condition
+        (BDD          : in out BDD_Type;
+         Condition_Id : SCO_Id);
+      --  Process condition
+
+      procedure Completed (BDD : in out BDD_Type);
+      --  Called when all items in decision have been processed
+
+      procedure Dump_BDD (BDD : BDD_Type);
+      --  Display BDD for debugging purposes
+
+   end BDD;
+
+   package body BDD is
+      package Arcs_Stacks is
+        new Ada.Containers.Vectors
+          (Index_Type   => Natural,
+           Element_Type => Arcs);
+
+      Arcs_Stack : Arcs_Stacks.Vector;
+
+      --------------
+      -- Allocate --
+      --------------
+
+      procedure Allocate
+        (BDD     : in out BDD_Type;
+         Node    : BDD_Node;
+         Node_Id : out BDD_Node_Id)
+      is
+      begin
+         BDD.V.Append (Node);
+         Node_Id := BDD.V.Last_Index;
+      end Allocate;
+
+      ---------------
+      -- Completed --
+      ---------------
+
+      procedure Completed (BDD : in out BDD_Type) is
+         use BDD_Vectors;
+
+         procedure Patch_Jumps (Node : in out BDD_Node);
+         --  Replace all destinations of Node that denote Jump nodes with
+         --  the jump destination.
+
+         -----------------
+         -- Patch_Jumps --
+         -----------------
+
+         procedure Patch_Jumps (Node : in out BDD_Node) is
+            procedure Patch_Jump (Dest : in out BDD_Node_Id);
+            --  If Dest denotes a Jump node, replace it with its destination
+
+            ----------------
+            -- Patch_Jump --
+            ----------------
+
+            procedure Patch_Jump (Dest : in out BDD_Node_Id) is
+               Dest_Node : constant BDD_Node := BDD.V.Element (Dest);
+            begin
+               if Dest_Node.Kind = Jump then
+                  Dest := Dest_Node.Dest;
+               end if;
+               pragma Assert (BDD.V.Element (Dest).Kind /= Jump);
+            end Patch_Jump;
+
+         begin
+            case Node.Kind is
+               when Jump =>
+                  Patch_Jump (Node.Dest);
+
+               when Condition =>
+                  Patch_Jump (Node.Dests.Dest_False);
+                  Patch_Jump (Node.Dests.Dest_True);
+
+               when others =>
+                  null;
+            end case;
+         end Patch_Jumps;
+
+         use type Ada.Containers.Count_Type;
+
+      --  Start of processing for Completed
+
+      begin
+         --  Check that all arcs have been consumed
+
+         pragma Assert (Arcs_Stack.Length = 0);
+
+         --  Check that the root condition has been set
+
+         pragma Assert (BDD.Root_Condition /= No_BDD_Node_Id);
+
+         --  Iterate backwards on BDD nodes, replacing references to jump nodes
+         --  with references to their destination.
+
+         for J in reverse BDD.V.First_Index .. BDD.V.Last_Index loop
+            BDD.V.Update_Element (J, Patch_Jumps'Access);
+         end loop;
+
+         if Verbose then
+            Dump_BDD (BDD);
+         end if;
+      end Completed;
+
+      --------------
+      -- Dump_BDD --
+      --------------
+
+      procedure Dump_BDD (BDD : BDD_Type) is
+         procedure Dump_Condition (N : BDD_Node_Id);
+         --  Display one condition
+
+         procedure Dump_Condition (N : BDD_Node_Id) is
+            use Ada.Strings;
+
+            Node : BDD_Node renames BDD.V.Element (N);
+            Next_Condition : BDD_Node_Id := N + 1;
+
+            procedure Put_Dest (Name : String; Dest : BDD_Node_Id);
+            --  Dump one destination
+
+            --------------
+            -- Put_Dest --
+            --------------
+
+            procedure Put_Dest (Name : String; Dest : BDD_Node_Id) is
+               Dest_Node : BDD_Node renames BDD.V.Element (Dest);
+            begin
+               Put ("    if " & Name & " then ");
+               case Dest_Node.Kind is
+                  when Exit_False =>
+                     Put_Line ("return False");
+                  when Exit_True =>
+                     Put_Line ("return True");
+                  when Condition =>
+                     if Dest = Next_Condition then
+                        Put_Line ("fallthrough");
+                     else
+                        Put_Line ("goto " & Dest'Img);
+                     end if;
+                  when others =>
+                     raise Program_Error with "malformed BDD";
+               end case;
+            end Put_Dest;
+
+         --  Start of processing for Dump_Condition
+
+         begin
+            pragma Assert (Node.Kind = Condition);
+
+            while Next_Condition <= BDD.V.Last_Index
+              and then BDD.V.Element (Next_Condition).Kind /= Condition
+            loop
+               Next_Condition := Next_Condition + 1;
+            end loop;
+
+            Put ("@" & Trim (N'Img, Side => Both)
+                 & ": test " & Image (Node.C_Sco));
+            case SCO_Vector.Element (Node.C_Sco).Value is
+               when False =>
+                  --  Static known False
+                  Put_Line (" (always False)");
+
+               when True =>
+                  --  Static known True
+                  Put_Line (" (always True)");
+
+               when Unknown =>
+                  --  Real runtime test
+                  New_Line;
+            end case;
+
+            Put_Dest ("true", Node.Dests.Dest_True);
+            Put_Dest ("false", Node.Dests.Dest_False);
+
+            if Next_Condition <= BDD.V.Last_Index then
+               New_Line;
+               Dump_Condition (Next_Condition);
+            end if;
+         end Dump_Condition;
+
+      --  Start of processing for Dump_BDD
+
+      begin
+         New_Line;
+         Put_Line ("----- BDD for decision " & Image (BDD.Decision));
+
+         Dump_Condition (BDD.Root_Condition);
+      end Dump_BDD;
+
+      ------------
+      -- Create --
+      ------------
+
+      function Create (Decision : SCO_Id) return BDD_Type is
+         Exit_False_Id, Exit_True_Id : BDD_Node_Id;
+      begin
+         return BDD : BDD_Type do
+            BDD.Decision := Decision;
+
+            Allocate (BDD,
+              BDD_Node'(Kind => Exit_False), Exit_False_Id);
+            Allocate (BDD,
+              BDD_Node'(Kind => Exit_True),  Exit_True_Id);
+
+            Push
+              (((Dest_False => Exit_False_Id,
+                 Dest_True  => Exit_True_Id),
+                Origin => No_BDD_Node_Id));
+         end return;
+      end Create;
+
+      ----------------------
+      -- Process_And_Then --
+      ----------------------
+
+      procedure Process_And_Then (BDD : in out BDD_Type) is
+         A : constant Arcs := Pop;
+         L : BDD_Node_Id;
+      begin
+         Allocate (BDD, BDD_Node'(Kind => Jump, Dest => No_BDD_Node_Id), L);
+
+         --  Arcs for right operand: subtree is reached through label L if
+         --  left operand is True.
+
+         Push
+           (((Dest_False => A.Dests.Dest_False,
+              Dest_True  => A.Dests.Dest_True),
+             Origin => L));
+
+         --  Arcs for left operand
+
+         Push
+           (((Dest_False => A.Dests.Dest_False,
+              Dest_True  => L),
+             Origin => A.Origin));
+      end Process_And_Then;
+
+      -----------------
+      -- Process_Not --
+      -----------------
+
+      procedure Process_Not (BDD : BDD_Type) is
+         pragma Unreferenced (BDD);
+
+         A : constant Arcs := Pop;
+      begin
+         --  Swap destinations of top arcs
+
+         Push
+           (((Dest_False => A.Dests.Dest_True,
+              Dest_True  => A.Dests.Dest_False),
+             Origin => A.Origin));
+      end Process_Not;
+
+      ---------------------
+      -- Process_Or_Else --
+      ---------------------
+
+      procedure Process_Or_Else (BDD : in out BDD_Type) is
+         A : constant Arcs := Pop;
+         L : BDD_Node_Id;
+      begin
+         Allocate (BDD, BDD_Node'(Kind => Jump, Dest => No_BDD_Node_Id), L);
+
+         --  Arcs for right operand: subtree is reached through label L if
+         --  left operand is False.
+
+         Push
+           (((Dest_False => A.Dests.Dest_False,
+              Dest_True  => A.Dests.Dest_True),
+             Origin => L));
+
+         --  Arcs for left operand
+
+         Push
+           (((Dest_False => L,
+              Dest_True  => A.Dests.Dest_True),
+             Origin => A.Origin));
+      end Process_Or_Else;
+
+      -----------------------
+      -- Process_Condition --
+      -----------------------
+
+      procedure Process_Condition
+        (BDD          : in out BDD_Type;
+         Condition_Id : SCO_Id)
+      is
+         A : constant Arcs := Pop;
+         N : BDD_Node_Id;
+      begin
+         Allocate (BDD,
+           (Kind => Condition, C_SCO => Condition_Id, Dests => A.Dests), N);
+
+         if A.Origin /= No_BDD_Node_Id then
+            declare
+               procedure Set_Dest (Origin_Node : in out BDD_Node);
+               --  Set destination of Origin_Node to N
+
+               --------------
+               -- Set_Dest --
+               --------------
+
+               procedure Set_Dest (Origin_Node : in out BDD_Node) is
+               begin
+                  Origin_Node.Dest := N;
+               end Set_Dest;
+            begin
+               BDD.V.Update_Element (A.Origin, Set_Dest'Access);
+            end;
+
+         else
+            pragma Assert (BDD.Root_Condition = No_BDD_Node_Id);
+            BDD.Root_Condition := N;
+         end if;
+      end Process_Condition;
+
+      ---------
+      -- Pop --
+      ---------
+
+      function Pop return Arcs is
+      begin
+         return Top : constant Arcs := Arcs_Stack.Last_Element do
+            Arcs_Stack.Delete_Last;
+         end return;
+      end Pop;
+
+      ----------
+      -- Push --
+      ----------
+
+      procedure Push (A : Arcs) is
+      begin
+         Arcs_Stack.Append (A);
+      end Push;
+
+   end BDD;
 
    --------------------------
    -- Sloc -> SCO_Id index --
@@ -194,6 +654,8 @@ package body SC_Obligations is
       Index : Natural;
 
       Current_Complex_Decision : SCO_Id := No_SCO_Id;
+      Current_BDD : BDD.BDD_Type;
+      --  BDD of current decision
 
       Last_SCO_Upon_Entry : constant SCO_Id := SCO_Vector.Last_Index;
 
@@ -275,6 +737,7 @@ package body SC_Obligations is
       Index := 1;
 
       Get_SCOs_From_ALI;
+      Close (ALI_File);
 
       --  Walk low-level SCO table for this unit and populate high-level tables
 
@@ -305,6 +768,9 @@ package body SC_Obligations is
             SCOE : SCOs.SCO_Table_Entry renames
                                      SCOs.SCO_Table.Table (Cur_SCO_Entry);
 
+            function Make_Condition_Value return Tristate;
+            --  Map condition value code (t/f/c) in SCOE.C2 to Tristate
+
             function Make_Sloc
               (SCO_Source_Loc : SCOs.Source_Location) return Source_Location;
             --  Build a Sources.Source_Location record from the low-level
@@ -313,6 +779,22 @@ package body SC_Obligations is
             procedure Update_Decision_Sloc (SCOD : in out SCO_Descriptor);
             --  Update the first sloc of a complex decision SCOD from that
             --  of its first condition (which is the current SCOE).
+
+            --------------------------
+            -- Make_Condition_Value --
+            --------------------------
+
+            function Make_Condition_Value return Tristate is
+            begin
+               case SCOE.C2 is
+                  when 'f' => return False;
+                  when 't' => return True;
+                  when 'c' => return Unknown;
+
+                  when others => raise Program_Error with
+                       "invalid SCO condition value code: " & SCOE.C2;
+               end case;
+            end Make_Condition_Value;
 
             ---------------
             -- Make_Sloc --
@@ -367,6 +849,7 @@ package body SC_Obligations is
                                      Is_Complex_Decision =>
                                                    not SCOE.Last,
                                      others     => <>));
+                  Current_BDD := BDD.Create (SCO_Vector.Last_Index);
 
                   if SCOE.Last then
                      --  Simple decision: no separate condition SCOE, create
@@ -377,7 +860,13 @@ package body SC_Obligations is
                                         First_Sloc => Make_Sloc (SCOE.From),
                                         Last_Sloc  => Make_Sloc (SCOE.To),
                                         Parent     => SCO_Vector.Last_Index,
+                                        Value      => Make_Condition_Value,
                                         others     => <>));
+                     BDD.Process_Condition
+                       (Current_BDD, SCO_Vector.Last_Index);
+                     BDD.Completed (Current_BDD);
+
+                     --  Reflect BDD info in SCO descriptors???
 
                   else
                      --  Complex decision: conditions appear as distinct SCOEs
@@ -399,16 +888,31 @@ package body SC_Obligations is
                                      First_Sloc => Make_Sloc (SCOE.From),
                                      Last_Sloc  => Make_Sloc (SCOE.To),
                                      Parent     => Current_Complex_Decision,
+                                     Value      => Make_Condition_Value,
                                      others     => <>));
+                  BDD.Process_Condition (Current_BDD, SCO_Vector.Last_Index);
 
                   if SCOE.Last then
+                     BDD.Completed (Current_BDD);
+
+                     --  Reflect BDD info in SCO descriptors???
+
                      Current_Complex_Decision := No_SCO_Id;
                   end if;
 
-               when '!' | '^' | '&' | '|' =>
-                  --  Operator
+               when '!' =>
+                  BDD.Process_Not (Current_BDD);
 
-                  null;
+               when '&' =>
+                  BDD.Process_And_Then (Current_BDD);
+
+               when '|' =>
+                  BDD.Process_Or_Else (Current_BDD);
+
+               when '^' =>
+                  raise Program_Error with
+                    "forbidden usage of XOR operator in decision: "
+                      & Image (Current_Complex_Decision);
 
                when 'T' =>
                   --  Exit point
@@ -419,7 +923,6 @@ package body SC_Obligations is
                   raise Program_Error
                     with "unexpected SCO entry code: " & SCOE.C1;
             end case;
-
          end Process_Entry;
       end loop;
 
@@ -483,8 +986,6 @@ package body SC_Obligations is
             SCO_Vector.Update_Element (J, Process_Descriptor'Access);
          end;
       end loop;
-
-      Close (ALI_File);
    end Load_SCOs_From_ALI;
 
    ------------------------------
