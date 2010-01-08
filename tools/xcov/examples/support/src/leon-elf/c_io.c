22f89619c236d5a97b9f0dfa867bbbc8813028c8
Tristan Gingold <gingold@adacore.com>
2010-01-08 15:35:26 +0000

    Implement getchar. Reformatting. Part of IC07-026.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2462 020d506d-db78-4e55-b2a8-6c851f84c332

--- tools/xcov/examples/support/src/leon-elf/c_io.c
+++ tools/xcov/examples/support/src/leon-elf/c_io.c
@@ -2,7 +2,7 @@
  *                                                                          *
  *                              Couverture                                  *
  *                                                                          *
- *                     Copyright (C) 2008-2009, AdaCore                     *
+ *                     Copyright (C) 2008-2010, AdaCore                     *
  *                                                                          *
  * Couverture is free software; you can redistribute it  and/or modify it   *
  * under terms of the GNU General Public License as published by the Free   *
@@ -22,51 +22,47 @@
 #define UART_CONTROL_ADDR(ADDR) ((volatile UART_Control_Register *)(ADDR))
 #define UART_SCALER_ADDR(ADDR) ((volatile UART_Scaler_Register *)(ADDR))
 
-typedef struct _UART_Data_Register{
-  int Reserved : 24;
+typedef struct _UART_Data_Register {
+  unsigned int Reserved : 24;
   unsigned char RTD;
 } UART_Data_Register;
 
-typedef struct _UART_Status_Register{
-  int Reserved: 25;
-  int FE : 1;
-  int PE : 1;
-  int OV : 1;
-  int BR : 1;
-  int TH : 1;
-  int TS : 1;
-  int DR : 1;
+typedef struct _UART_Status_Register {
+  unsigned int Reserved: 25;
+  unsigned int FE : 1;
+  unsigned int PE : 1;
+  unsigned int OV : 1;
+  unsigned int BR : 1;
+  unsigned int TH : 1;
+  unsigned int TS : 1;
+  unsigned int DR : 1;
 } UART_Status_Register;
 
-typedef struct _UART_Control_Register{
-  int Reserved: 23;
-  int EC : 1;
-  int LB : 1;
-  int FL : 1;
-  int PE : 1;
-  int PS : 1;
-  int TI : 1;
-  int RI : 1;
-  int TE : 1;
-  int RE : 1;
+typedef struct _UART_Control_Register {
+  unsigned int Reserved: 23;
+  unsigned int EC : 1;
+  unsigned int LB : 1;
+  unsigned int FL : 1;
+  unsigned int PE : 1;
+  unsigned int PS : 1;
+  unsigned int TI : 1;
+  unsigned int RI : 1;
+  unsigned int TE : 1;
+  unsigned int RE : 1;
 } UART_Control_Register;
 
-typedef struct _UART_Scaler_Register{
-  int Reserved : 20;
-  int UART_Scaler : 12;
+typedef struct _UART_Scaler_Register {
+  unsigned int Reserved : 20;
+  unsigned int UART_Scaler : 12;
 } UART_Scaler_Register;
 
-volatile UART_Data_Register *
-UART1_Data_ptr = UART_DATA_ADDR (0x80000070);
+static volatile UART_Data_Register * const UART1_Data_ptr = UART_DATA_ADDR (0x80000070);
 
-volatile UART_Status_Register *
-UART1_Status_ptr = UART_STATUS_ADDR (0x80000074);
+static volatile UART_Status_Register * const UART1_Status_ptr = UART_STATUS_ADDR (0x80000074);
 
-volatile UART_Control_Register *
-UART1_Control_ptr = UART_CONTROL_ADDR (0x80000078);
+static volatile UART_Control_Register * const UART1_Control_ptr = UART_CONTROL_ADDR (0x80000078);
 
-volatile UART_Scaler_Register *
-UART1_Scaler_ptr = UART_SCALER_ADDR (0x8000007c);
+static volatile UART_Scaler_Register * const UART1_Scaler_ptr = UART_SCALER_ADDR (0x8000007c);
 
 #define UART_1_DATA_REG (*UART1_Data_ptr)
 #define UART_1_STATUS_REG (*UART1_Status_ptr)
@@ -76,18 +72,29 @@ UART1_Scaler_ptr = UART_SCALER_ADDR (0x8000007c);
 int putchar(int c)
 {
   UART_Data_Register UART_Tx;
-  UART_Tx.RTD = c;
   UART_Status_Register UART_Status_Aux;
-  do{
+
+  UART_Tx.RTD = c;
+
+  do
     UART_Status_Aux = UART_1_STATUS_REG;
-  }while(!(UART_Status_Aux.TH & 1));
+  while (!UART_Status_Aux.TH);
   UART_1_DATA_REG = UART_Tx;
   return c;
 }
 
 int getchar (void)
 {
-  return 0;
+  UART_Data_Register UART_Rx;
+  UART_Status_Register UART_Status_Aux;
+
+  do
+    UART_Status_Aux = UART_1_STATUS_REG;
+  while(!UART_Status_Aux.DR);
+
+  UART_Rx = UART_1_DATA_REG;
+
+  return UART_Rx.RTD;
 }
 
 void abort (void)
