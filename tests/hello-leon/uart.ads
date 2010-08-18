7dee986d3a845ed0231d3af437d602f10c2f784e
Olivier Hainque <hainque@adacore.com>
2010-08-18 10:27:05 +0000

    Remove obsolete 'tests' directory, in preparation of the addition of a 'testsuite' alternative.
    H730-024
    
    
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2542 020d506d-db78-4e55-b2a8-6c851f84c332

--- tests/hello-leon/uart.ads
+++
@@ -1,9 +0,0 @@
-package Uart is
-   procedure Put (C : Character);
-
-   procedure New_Line;
-   procedure Put_Line (Str : String);
-   procedure Put (Str : String);
-
-   procedure Get (C : out Character);
-end Uart;
