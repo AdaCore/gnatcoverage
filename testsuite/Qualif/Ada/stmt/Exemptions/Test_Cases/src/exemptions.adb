f638d97a9b646552fbe7c178bc97a47e5847b179
Olivier Hainque <hainque@adacore.com>
2010-11-11 07:51:14 +0000

    Install placeholder for testcase on exemptions (J507-015)
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2654 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/Exemptions/Test_Cases/src/exemptions.adb
+++ testsuite/Qualif/Ada/stmt/Exemptions/Test_Cases/src/exemptions.adb
@@ -14,9 +14,11 @@ package body Exemptions is
       if X = 1 then                                        -- # factorial
          return 1;                                         -- # 1_factorial
       elsif X >= Positive'Last / 1000 then                 -- # elsif_factorial
-         pragma Annotate (Xcov, Exempt_On, "exemption on statements in function");
+         pragma Annotate                                   -- # ex_factorial
+           (Xcov, Exempt_On,                               -- # ex_factorial
+              "exemption on statements in function");      -- # ex_factorial
          return Positive'Last;                             -- # ex_factorial
-         pragma Annotate (Xcov, Exempt_Off);
+         pragma Annotate (Xcov, Exempt_Off);               -- # ex_factorial
       else
          return X * Factorial (X - 1);                     -- # rec_factorial
       end if;
@@ -30,31 +32,35 @@ package body Exemptions is
    end Another_Swap;
 
    function Another_Factorial (X : Natural) return Positive is
-      Result : Positive := 1;                              -- # another_factorial
+      Result : Positive := 1;                   -- # another_factorial
    begin
-      for J in 1 .. X loop                                 -- # another_factorial
-         if Result > Positive'Last / J then                -- # in_loop_a_f
-            raise Constraint_Error;                        -- # in_if_a_f
+      for J in 1 .. X loop                      -- # another_factorial
+         if Result > Positive'Last / J then     -- # in_loop_a_f
+            raise Constraint_Error;             -- # in_if_a_f
          end if;
 
-         Result := Result * J;                             -- # in_loop_a_f
+         Result := Result * J;                  -- # in_loop_a_f
       end loop;
 
-      return Result;                                       -- # another_factorial
+      return Result;                            -- # another_factorial
    exception
       when Constraint_Error =>
-         return Positive'Last;                             -- # handler_a_f
+         return Positive'Last;                  -- # handler_a_f
    end Another_Factorial;
 
 
 begin
-   pragma Annotate (Xcov, Exempt_On, "exemption on elaboration code - 1");
-   Z := Identity (3);                                      -- # 1_elab
-   pragma Annotate (Xcov, Exempt_Off);
+   pragma Annotate                            -- # 1_elab
+     (Xcov, Exempt_On,                        -- # 1_elab
+        "exemption on elaboration code - 1"); -- # 1_elab
+   Z := Identity (3);                         -- # 1_elab
+   pragma Annotate (Xcov, Exempt_Off);        -- # 1_elab
 
-   Another_Swap (X, Y);                                    -- # elab
+   Another_Swap (X, Y);                       -- # elab
 
-   pragma Annotate (Xcov, Exempt_On, "exemption on elaboration code - 2");
-   Another_Swap (Z, X);                                    -- # 2_elab
-   pragma Annotate (Xcov, Exempt_Off);
+   pragma Annotate                            -- # 2_elab
+     (Xcov, Exempt_On,                        -- # 2_elab
+        "exemption on elaboration code - 2"); -- # 2_elab
+   Another_Swap (Z, X);                       -- # 2_elab
+   pragma Annotate (Xcov, Exempt_Off);        -- # 2_elab
 end Exemptions;
