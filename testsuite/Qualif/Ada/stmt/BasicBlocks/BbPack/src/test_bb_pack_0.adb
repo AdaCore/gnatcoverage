04440fc39d6df63bf7f4485da690c16daaccf9a8
Olivier Hainque <hainque@adacore.com>
2010-11-14 14:01:00 +0000

    Fix main suprogram name to match unit name. H407-002.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2678 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/BasicBlocks/BbPack/src/test_bb_pack_0.adb
+++ testsuite/Qualif/Ada/stmt/BasicBlocks/BbPack/src/test_bb_pack_0.adb
@@ -3,7 +3,7 @@ with Support, BB_Pack; use Support;
 -- "with" the package featuring elaboration code only and verify that
 -- nothing is reported uncovered.
 
-procedure Test_BB_Pack is
+procedure Test_BB_Pack_0 is
 begin
    Assert (BB_Pack.X = 0);
 end;
