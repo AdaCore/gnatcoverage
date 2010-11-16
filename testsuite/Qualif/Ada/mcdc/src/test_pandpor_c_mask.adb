4d39889391872eeba70ff3c72b833645c0e7b5ce
Jerome Guitton <guitton@adacore.com>
2010-11-16 16:05:33 +0000

    Add testcases that show a difference between Masking MC/DC and Unique Cause
    Part of JB12-009.
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2687 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/mcdc/src/test_pandpor_c_mask.adb
@@ -0,0 +1,16 @@
+with Support, PandPor; use Support, PandPor;
+
+--  C covered for Masking MC/DC only
+
+procedure Test_PandPor_C_Mask is
+begin
+   Assert (F (True, False, True) = True);
+   Assert (F (False, True, False) = False);
+end;
+
+--# pandpor.adb
+-- /eval(Stmt|Other)/      l! c!:"A",c!:"B",c!:"C"
+-- /decisionTrue/  l+ 0
+-- /decisionFalse/ l+ 0
+-- /returnValue/   l+ 0
+
---
+++ testsuite/Qualif/Ada/mcdc/src/test_porpandporp_abcd_mask.adb
@@ -0,0 +1,23 @@
+with Support, PorPandPorP;
+use Support;
+
+--  driver for (A or else B) and then (C or else D)
+--
+-- covering all conditions for Masking MC/DC; D not covered for Unique Cause.
+-- A : {1, 4}
+-- B : {1, 2}
+-- C : {2, 3}
+-- D : {3, 4}
+procedure Test_PorPandPorP_ABCD_Mask is
+begin
+   Assert (PorPandPorP.F (False, False, True, True) = False); -- 1
+   Assert (PorPandPorP.F (False, True, True, False) = True); -- 2
+   Assert (PorPandPorP.F (False, True, False, False) = False); -- 3
+   Assert (PorPandPorP.F (True, False, False, True) = True); --  4
+end Test_PorPandPorP_ABCD_Mask;
+
+--# porpandporp.adb
+--  /eval(Stmt|Other)/  l!  c!:"D"
+--  /decisionTrue/  l+ 0
+--  /decisionFalse/ l+ 0
+--  /returnValue/ l+ 0
