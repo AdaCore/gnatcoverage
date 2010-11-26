12318fa1726b702de825a42542f35459fe187607
Olivier Hainque <hainque@adacore.com>
2010-11-26 13:16:44 +0000

    Part of feorg Ada/decision qualification tests subdir for Couverture. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2767 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/decision/IsolatedConstructs/src/test_andthen_b.adb
@@ -0,0 +1,13 @@
+with Support, Andthen; use Support, Andthen;
+
+procedure Test_Andthen_B is
+begin
+   Assert (And_Then (True, True) = True);
+   Assert (And_Then (True, False) = False);
+end;
+
+--# andthen.adb
+--  /andthen/  l+ 0
+--  /retTrue/  l+ 0
+--  /retFalse/ l+ 0
+--  /retVal/   l+ 0
