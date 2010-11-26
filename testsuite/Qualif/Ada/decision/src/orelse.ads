b861afada00241f30dc548877c3decfe53554948
Olivier Hainque <hainque@adacore.com>
2010-11-26 13:55:24 +0000

    Part of reorg Ada/decision qualification tests subdir for Couverture. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2771 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/src/orelse.ads
+++
@@ -1,3 +0,0 @@
-package Orelse is
-   function Or_Else (A, B : Boolean) return Boolean;
-end;
--- testsuite/Qualif/Ada/decision/support.ads
+++
@@ -1,6 +0,0 @@
-
-package Support is
-   procedure Assert (Cond : Boolean);
-   function Identity (X : Integer) return Integer;
-   function Identity (B : Boolean) return Boolean;
-end;
