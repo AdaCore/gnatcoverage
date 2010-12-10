d2d0a0c0431439a78419d85b56763432013abe46
Olivier Hainque <hainque@adacore.com>
2010-12-10 16:17:26 +0000

    Delete directory dedicated to return statements. Testcases now split across expression subtrees. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2798 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/IsolatedConstructs/ShapesAndContexts/ReturnS/src/andthen.adb
+++
@@ -1,7 +0,0 @@
-package body Andthen is
-   function And_Then (A, B : Boolean) return Boolean is
-   begin
-      return A and then B;  -- # andthen
-   end;
-end;
-
--- testsuite/Qualif/Ada/decision/IsolatedConstructs/ShapesAndContexts/ReturnS/test.py
+++
@@ -1,5 +0,0 @@
-from test_utils import *
-
-ExerciseAll(
-    extradrivers="../../src/test_andthen*.adb ../../src/test_orelse*.adb")
-thistest.result()
