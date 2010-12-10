94f307f6be3a0f9364ef9159505df10aaaa2d9ca
Olivier Hainque <hainque@adacore.com>
2010-12-10 16:42:28 +0000

    More reorg of the ShapesAndContext section of the decision coverage suite for xcov. H407-002.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2800 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/IsolatedConstructs/ShapesAndContexts/Value/Case/src/value.adb
+++
@@ -1,9 +0,0 @@
-package body Value is
-   function F (X : Boolean) return Boolean is
-   begin
-      case X is                      -- # evaluate
-         when True  => return True;  -- # returnTrue
-         when False => return False; -- # returnFalse
-      end case;
-   end;
-end;
