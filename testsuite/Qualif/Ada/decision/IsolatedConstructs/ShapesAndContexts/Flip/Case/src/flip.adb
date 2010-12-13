66acd66ef8140492e67c76cc0c6a4cf1ea701470
Olivier Hainque <hainque@adacore.com>
2010-12-13 09:37:02 +0000

    Further reorg and updates for H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2802 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/IsolatedConstructs/ShapesAndContexts/Flip/Case/src/flip.adb
+++
@@ -1,9 +0,0 @@
-package body Flip is
-   function F (X : Boolean) return Boolean is
-   begin
-      case not X is                    -- # evaluate
-         when True  => return True;    -- # returnTrue
-         when False => return False;   -- # returnFalse
-      end case;
-   end;
-end;
