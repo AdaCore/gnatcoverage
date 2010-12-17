7699718520e53788ff398e988e21a3bcf8a5a441
Olivier Hainque <hainque@adacore.com>
2010-12-17 11:16:59 +0000

    Revert to a decision expression that causes unexpected failure for JC16-020.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2817 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/IsolatedConstructs/Preconditions/SingleCond/src/points.adb
+++ testsuite/Qualif/Ada/decision/IsolatedConstructs/Preconditions/SingleCond/src/points.adb
@@ -3,7 +3,7 @@ pragma Check_Policy (Precondition, On);
 package body Points is
 
    procedure Set (P : out Point; X, Y : Integer) is
-      pragma Precondition (P.Set = False); -- # preSet
+      pragma Precondition (not P.Set); -- # preSet
    begin
       P := (X => X, Y => Y, Set => True); -- # bodySet
    end;
