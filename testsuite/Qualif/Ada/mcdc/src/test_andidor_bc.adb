1f552af28258167499e04118d36d18156ab24f35
Olivier Hainque <hainque@adacore.com>
2010-12-22 15:32:37 +0000

    Adjust expectations on a couple of mcdc coverage tests, after introduction of a new diagnostic in JC16-020
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2830 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/mcdc/src/test_andidor_bc.adb
+++ testsuite/Qualif/Ada/mcdc/src/test_andidor_bc.adb
@@ -8,7 +8,7 @@ begin
 end;
 
 --# andidor.adb
--- /eval(Stmt|Other)/      l! dT-:"A"
+-- /eval(Stmt|Other)/      l! dT-:"A",d-:"B"
 -- /decisionTrue/  l- s-
 -- /decisionFalse/ l+ 0
 -- /returnValue/   l+ 0
