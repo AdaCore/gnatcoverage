3ff7b7176488cedbcba60b7b0f070ef619197170
Olivier Hainque <hainque@adacore.com>
2011-01-13 14:46:43 +0000

    File missing mcdc testcases and descriptions in ShapesAndContexts/AndpOrp section. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2909 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/mcdc/1_Core/ShapesAndContexts/AndpOrp/Aggregate/src/andporp.adb
@@ -0,0 +1,12 @@
+package body AndPorP is
+
+   type Expr is record
+      Value : Boolean;
+   end record;
+
+   function F (A, B, C : Boolean) return Boolean is
+      E : Expr := (Value => (A and then (B or else C))); -- # evalStmt
+   begin
+      return E.Value;  -- # returnValue
+   end;
+end;
