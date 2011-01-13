c7f0d33187da8141d2ad5a0037711d074381c574
Olivier Hainque <hainque@adacore.com>
2011-01-13 13:58:43 +0000

    File missing mcdc testcases and descriptions in ShapesAndContexts/AndCOr section. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2906 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/mcdc/1_Core/ShapesAndContexts/AndCOr/Aggregate/src/andcor.adb
@@ -0,0 +1,28 @@
+package body Andcor is
+
+   type Expr is record
+      Value : Boolean;
+   end record;
+
+   function Orelse (B, C : Boolean) return Boolean is
+   begin
+      return B or else C; -- # orelse
+   end;
+
+   procedure Eval_F (A, B, C : Boolean; E : out Boolean) is
+      Ex : Expr := (Value => A and then Orelse (B, C));  -- # andthen
+   begin
+      E := Ex.Value;  -- # returnValue
+   end;
+
+   function F (A, B, C : Boolean) return Boolean is
+      Value : Boolean;
+   begin
+      Eval_F (A, B, C, Value); -- # returnValue
+      return Value;            -- # returnValue
+   end;
+
+end;
+
+
+
---
+++ testsuite/Qualif/Ada/mcdc/1_Core/ShapesAndContexts/AndCOr/DeclInit/src/andcor.adb
@@ -0,0 +1,24 @@
+package body AndCor is
+
+   function Orelse (B, C : Boolean) return Boolean is
+   begin
+      return B or else C; -- # orelse
+   end;
+
+   procedure Eval_F (A, B, C : Boolean; E : out Boolean) is
+      R : Boolean := A and then Orelse (B, C);  -- # andthen
+   begin
+      E := R;  -- # returnValue
+   end;
+
+   function F (A, B, C : Boolean) return Boolean is
+      Value : Boolean;
+   begin
+      Eval_F (A, B, C, Value); -- # returnValue
+      return Value;            -- # returnValue
+   end;
+
+end;
+
+
+
