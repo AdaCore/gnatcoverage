2e764b003af1eafedc0f1b68b5972fb3f4240a5b
Olivier Hainque <hainque@adacore.com>
2011-01-14 17:52:55 +0000

    File material for mcdc exemption test.  H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2913 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/mcdc/Exemptions/PrecondInBody/src/test_ranges_invalid_b.adb
@@ -0,0 +1,25 @@
+with Ranges, Support, Silent_Last_Chance; use Ranges, Support;
+
+-- Exercise a single overlap check, involving an invalid range B.
+-- Exempted precondition evaluated False only.
+
+procedure Test_Ranges_Invalid_B is
+   Ra, Rb : XYrange;
+   Ov : Boolean;
+begin
+   Set (Ra, 1, 2);
+   Set (Rb, 4, 1); -- invalid
+
+   --  One call to Overlap, with invalid range Rb.
+   --  Invoke silent last chance handler.
+
+   Ov := Overlap (Ra, Rb);
+end;
+
+--# ranges.adb
+--  /checkValid/    l+ 0
+--  /assignValid/   l+ 0
+--  /assignInvalid/ l+ 0
+--  /preValid/      l* x+
+--  /checkOverlap/  l- s-
+
