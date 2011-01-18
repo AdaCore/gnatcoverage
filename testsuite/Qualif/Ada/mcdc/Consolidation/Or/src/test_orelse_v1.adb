b43a5181881b93314a570eeee2359247edf2bc5d
Olivier Hainque <hainque@adacore.com>
2011-01-18 15:56:36 +0000

    File missing sources for Consolidation section in mcdc qualification material for xcov. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2922 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/mcdc/Consolidation/Or/src/test_orelse_v1.adb
@@ -0,0 +1,11 @@
+with Support, Orelse; use Support;
+
+procedure Test_Orelse_V1 is
+begin
+   Assert (Orelse (False, True) = True);
+end;
+
+--# orelse.adb
+--  /eval/ l! dF-
+
+
---
+++ testsuite/Qualif/Ada/mcdc/Consolidation/Or/src/test_orelse_v2.adb
@@ -0,0 +1,11 @@
+with Support, Orelse; use Support;
+
+procedure Test_Orelse_V2 is
+begin
+   Assert (Orelse (True, False) = True);
+end;
+
+--# orelse.adb
+--  /eval/ l! dF-
+
+
