b0148203ba9a38ece9b057a74abf759254e8ee24
Matteo Bordin <bordin@adacore.com>
2011-02-07 16:43:23 +0000

    moving
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2992 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/stmt/1_Core/SimpleStatements/Exit/src/exit_statements_support.ads
@@ -0,0 +1,10 @@
+--  Contains declarations needed for EXIT statement tests
+
+package EXIT_Statements_Support is
+
+   type Array_Sample is array (Natural range <>) of Integer;
+
+   type Matrix_Sample is array (Natural range <>, Natural range <>) of Integer;
+
+end EXIT_Statements_Support;
+
