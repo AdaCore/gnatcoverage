b4afcb5524615240f43196b0f52332129b3e161d
Olivier Hainque <hainque@adacore.com>
2011-02-07 10:46:05 +0000

    Restore accidental clobbering of functional source in DC testcase. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2978 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/1_Core/ShapesAndContexts/Value/If/src/value.adb
+++ testsuite/Qualif/Ada/decision/1_Core/ShapesAndContexts/Value/If/src/value.adb
@@ -1,17 +1,10 @@
 package body Value is
-   function Dovalue (X, Bomb : Boolean) is
-   begin
-      if Bomb then            -- # __l!d!
-         raise Program_Error; -- # __l-s-
-      elsif X then       -- # evaluate
-         return True;    -- # returnTrue
-      else
-         return False;   -- # returnFalse
-      end if;
-   end;
-
    function F (X : Boolean) return Boolean is
    begin
-      return Dovalue (X, Bomb => False); -- # returnVal
+      if X then        -- # evaluate
+         return True;  -- # returnTrue
+      else
+         return False; -- # returnFalse
+      end if;
    end;
 end;
