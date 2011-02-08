f388c5338a7dba7381dea432748238f5a1366d6c
Olivier Hainque <hainque@adacore.com>
2011-02-08 11:23:34 +0000

    Commit If_For_If_Case MixedConstructs testcase variation for the DC criterion.  H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@2999 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/decision/MixedConstructs/If_For_If_Case/src/test_broken.adb
@@ -0,0 +1,43 @@
+with Support; use Support;
+with Sensors.Status; use Sensors, Sensors.Status;
+
+procedure Test_Broken is
+   S : aliased Sensor (Hist_Size => 5);
+   
+begin
+   S.ALB := 1;
+   S.AHB := 10;
+   
+   --  Two history entries, both out of range
+   
+   S.V := 15;   
+   Sample (S);
+   Sample (S);
+   
+   Assert (Status_Of (S) = Broken);
+      
+   --  A third entry, in range. Still two out of range.
+   
+   S.V := 5;   
+   Sample (S);
+   
+   Assert (Status_Of (S) = Broken);
+end;
+
+-- Note that we get into the Check state internally, before
+-- moving to Broken.
+
+--# sensors-status.adb
+
+-- /SO_t0/       l! d!
+-- /SO_undecide/ l- s-
+-- /SO_decide/   l+ 0
+-- /SO_loop0/    l+ 0
+-- /SO_loop1/    l+ 0
+-- /SO_tfaultLB/ l+ 0
+-- /SO_tfaultHB/ l+ 0
+-- /SO_fault/    l+ 0
+-- /SO_check/    l+ 0
+-- /SO_broken/   l+ 0
+-- /SO_PE/       l- s-
+-- /SO_ret/      l+ 0
---
+++ testsuite/Qualif/Ada/decision/MixedConstructs/If_For_If_Case/src/test_check.adb
@@ -0,0 +1,39 @@
+with Support; use Support;
+with Sensors.Status; use Sensors, Sensors.Status;
+
+procedure Test_Check is
+   S : aliased Sensor (Hist_Size => 5);
+   
+begin
+   S.ALB := 1;
+   S.AHB := 10;
+   
+   --  Single history entry, out of range
+   
+   S.V := 15;   
+   Sample (S);
+   
+   Assert (Status_Of (S) = Check);
+   
+   --  Two history entries, only one out of range
+   
+   S.V := 5;   
+   Sample (S);
+   
+   Assert (Status_Of (S) = Check);
+end;
+
+--# sensors-status.adb
+
+-- /SO_t0/       l! d!
+-- /SO_undecide/ l- s-
+-- /SO_decide/   l+ 0
+-- /SO_loop0/    l+ 0
+-- /SO_loop1/    l+ 0
+-- /SO_tfaultLB/ l+ 0
+-- /SO_tfaultHB/ l+ 0
+-- /SO_fault/    l+ 0
+-- /SO_check/    l+ 0
+-- /SO_broken/   l- s-
+-- /SO_PE/       l- s-
+-- /SO_ret/      l+ 0
