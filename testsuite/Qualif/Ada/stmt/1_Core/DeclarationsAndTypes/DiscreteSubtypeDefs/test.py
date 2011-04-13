7a1bed2fd454c8c70f6a4105416f2080ef6e42c2
Olivier Hainque <hainque@adacore.com>
2011-04-13 08:14:24 +0000

    Remove obsolete sequences from test.py scripts for Couverture qualification tests. Preparatory cleanups for K407-019.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3105 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/1_Core/DeclarationsAndTypes/DiscreteSubtypeDefs/test.py
+++ testsuite/Qualif/Ada/stmt/1_Core/DeclarationsAndTypes/DiscreteSubtypeDefs/test.py
@@ -1,14 +1,3 @@
-from gnatpython.env import Env
-import os
-
-cwd = os.getcwd()
-root = 'tests' + os.sep + 'CouvertureQualification'
-rootPath = cwd[2:cwd.find (root)-1]
-rootPath = rootPath.replace (os.sep, '/')
-env = Env()
-#env.add_search_path('PYTHONPATH', rootPath)
-print rootPath
-
 from SCOV.tc import *
 
 TestCase().run()
