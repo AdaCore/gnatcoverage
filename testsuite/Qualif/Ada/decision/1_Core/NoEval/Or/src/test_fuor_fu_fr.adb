55c53f6d1400a2257bc74726e085bba602839315
Olivier Hainque <hainque@adacore.com>
2011-05-17 07:45:34 +0000

    Fix procedure name. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3183 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/decision/1_Core/NoEval/Or/src/test_fuor_fu_fr.adb
+++ testsuite/Qualif/Ada/decision/1_Core/NoEval/Or/src/test_fuor_fu_fr.adb
@@ -1,6 +1,6 @@
 with Silent_Last_Chance, Support, FUOR_Helper; use Support;
 
-procedure Test_FUOR_FU_TR is
+procedure Test_FUOR_FU_FR is
 begin
    FUOR_Helper.Eval_FF_F;
    FUOR_Helper.Eval_TX_T;
