c17c456ec4b19cbb95e60ee0f80cab45e117f29a
Olivier Hainque <hainque@adacore.com>
2011-05-27 10:38:16 +0000

    File extra drivers for DC assessment of and-then in Precondition, exercising precondition failures. H407-002
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3196 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ testsuite/Qualif/Ada/decision/1_Core/Contexts/Expr/And/Preconditions/src/test_fail_xy.adb
@@ -0,0 +1,29 @@
+with Support, Points, Silent_Last_Chance; use Support, Points;
+
+--  Call Same_X and Same_Y with regular arguments. Call Same_XY with an
+--  unset second point, causing a precondition failure and only that for
+--  the associated expression.
+
+--  Verify full stmt coverage nevertheless, except for the Same_XY body.
+
+procedure Test_Fail_XY is
+   P1, P2 : Point;
+begin
+   Set (P1, X => 0, Y => 1);
+
+   Assert (Same_X (P1, P1));
+   Assert (Same_Y (P1, P1));
+
+   Assert (Same_XY (P1, P2));
+end;
+
+--# points.adb
+--  /bodySX/   l+ 0
+--  /bodySY/   l+ 0
+--  /bodySet/  l+ 0
+--  /preSXY/   l+ 0
+--  /retSXY/   l- s-
+
+--# points.ads
+--  /preSX/    l+ 0
+--  /preSY/    l+ 0
