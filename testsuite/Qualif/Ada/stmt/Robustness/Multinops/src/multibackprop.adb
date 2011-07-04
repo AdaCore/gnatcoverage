77f8d7fa1cc00cf353c25d94c93558a4bcefe6f3
Olivier Hainque <hainque@adacore.com>
2011-07-04 09:50:06 +0000

    Add comments to source for TC on backprogagation, recently updated for J929-030.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3354 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/Robustness/Multinops/src/multibackprop.adb
+++ testsuite/Qualif/Ada/stmt/Robustness/Multinops/src/multibackprop.adb
@@ -1,5 +1,10 @@
 package body MultibackProp is
+
    procedure Q is
+      --  Dummy subprogram called from P to provide a backpropagation
+      --  start anchor. Make sure there is code associated with it, to
+      --  prevent the call from being just not emitted at all.
+
       X : Integer;
       pragma Volatile (X);
    begin
