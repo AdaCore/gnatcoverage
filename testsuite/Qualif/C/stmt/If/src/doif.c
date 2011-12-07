ff0db1e1cf0bde6afda055fbd4dedcaeaabb7b3c
Thomas Quinot <quinot@adacore.com>
2011-12-07 15:21:55 +0000

    Prevent code from being eliminated at -O1 (&= would never change *status since it has already been zeroed out!) For K427-026
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3806 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/C/stmt/If/src/doif.c
+++ testsuite/Qualif/C/stmt/If/src/doif.c
@@ -5,8 +5,8 @@ void doif (int cond, int * xstatus)
 {
   *xstatus = 0;           // # body
   if (cond)               // # eval
-    *xstatus &= XST_IF;   // # if
+    *xstatus |= XST_IF;   // # if
   else
-    *xstatus &= XST_ELSE; // # else
+    *xstatus |= XST_ELSE; // # else
 }
 
