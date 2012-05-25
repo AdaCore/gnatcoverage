ef1a0d0c2c35847c357424de45beb5aa5860b8e7
Olivier Hainque <hainque@adacore.com>
2012-05-25 21:35:53 +0000

    Commit getting started example in the examples subdir. L524-045
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@3952 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ tools/gnatcov/examples/starter/src/ops.ads
@@ -0,0 +1,22 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                               GNATcoverage                               --
+--                                                                          --
+--                       Copyright (C) 2012, AdaCore                        --
+--                                                                          --
+-- GNATcoverage is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+package Ops is
+   type Op_Kind is (Increment, Decrement);
+   procedure Apply (Op : Op_Kind; X : in out Integer);
+end Ops;
---
+++ tools/gnatcov/examples/starter/src/test_inc.adb
@@ -0,0 +1,25 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                               GNATcoverage                               --
+--                                                                          --
+--                       Copyright (C) 2012, AdaCore                        --
+--                                                                          --
+-- GNATcoverage is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ops;
+procedure Test_Inc is
+   X : Integer := 4;
+begin
+   Ops.Apply (Ops.Increment, X);
+   pragma Assert (X = 5);
+end Test_Inc;
