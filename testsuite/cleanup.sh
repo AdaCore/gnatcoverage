1e634f8c992781e41399e07da7a0f9acd41df1f5
Olivier Hainque <hainque@adacore.com>
2012-06-21 14:52:43 +0000

    Improve filtering to prevent accidental deletion of svn related files.
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4003 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/cleanup.sh
+++ testsuite/cleanup.sh
@@ -1,9 +1,20 @@
 # run this to remove most temp artifacts created
 # by the testsuite execution or ongoing development.
 
-rm -rf $(find -type d -name 'tmp_*')
-rm -rf $(find -type d -name '[0-9]')
-rm -rf $(find -type f -name 'test.py.???')
-rm -rf $(find -type f -name '*~')
+LIST=""
+LIST="$LIST $(find -type d -name 'tmp_*')"
+LIST="$LIST $(find -type d -name '[0-9]')"
+rm -rf $LIST
+
+LIST=""
+LIST="$LIST $(find -type d -name 'obj')"
+rm -rf $LIST
+
+LIST=""
+LIST="$LIST $(find -type f -name 'test.py.???')"
+LIST="$LIST $(find -type f -name '*~')"
+LIST="$LIST $(find -type f -name '*.adb.*')"
+rm -rf $(echo $LIST | sed -e 's/[^ ]*svn[^ ]*//g')
+
 
 
