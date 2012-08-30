c8d5018e1ad56425b3a9dc3c5b14fcb8f377f02d
Olivier Hainque <hainque@adacore.com>
2012-08-30 11:10:19 +0000

    Adjust Qualif/Ada/mcdc/1_Core/Operands/Attributes not to rely on malloc, which is unavailable in pure ZFP runtimes. Part of L814-007.
    The test currently uses explicit allocators to produce
    designated strings without any particular reason wrt
    the testcase intent and description. This change modifies
    the test to use 'access on straight String objects instead.
    
    
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4068 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/mcdc/1_Core/Operands/Attributes/src/fuor_helper.adb
+++ testsuite/Qualif/Ada/mcdc/1_Core/Operands/Attributes/src/fuor_helper.adb
@@ -4,32 +4,32 @@ package body FUOR_Helper is
 
    --  Strings of length 0, 1 and 2
 
-   S0 : String_Access := new String (1 .. 0);
-   S1 : String_Access := new String (5 .. 5);
-   S2 : String_Access := new String (1 .. 2);
+   S0 : aliased String := "";
+   S1 : aliased String := (5 .. 5 => '1');
+   S2 : aliased String := (1 .. 2 => '2');
 
    procedure Eval_FF_F is
    begin
       -- A not Empty, A'Length not = B'Length
-      Assert (not Empty_Or_Eql ((A => S2, B => S1)));
+      Assert (not Empty_Or_Eql ((A => S2'Access, B => S1'Access)));
    end;
 
    procedure Eval_FT_T is
    begin
       -- A not Empty, A'Length = B'Length
-      Assert (Empty_Or_Eql ((A => S1, B => S1)));
+      Assert (Empty_Or_Eql ((A => S1'Access, B => S1'Access)));
    end;
 
    procedure Eval_TF_T is
    begin
       -- A Empty, A'Length not = B'Length
-      Assert (Empty_Or_Eql ((A => S0, B => S1)));
+      Assert (Empty_Or_Eql ((A => S0'Access, B => S1'Access)));
    end;
 
    procedure Eval_TT_T is
    begin
       -- A Empty, A'Length = B'Length
-      Assert (Empty_Or_Eql ((A => S0, B => S0)));
+      Assert (Empty_Or_Eql ((A => S0'Access, B => S0'Access)));
    end;
 
 end;
