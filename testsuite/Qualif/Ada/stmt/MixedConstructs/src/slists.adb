1001c24674b3660354d6f773f0fb1fca44cb2842
Olivier Hainque <hainque@adacore.com>
2012-08-31 08:15:18 +0000

    Adjust the MixedConstructs testcases to use the basic allocator provided by our support module instead of a standard Ada allocator. The latter is not available in some runtime profiles which the former now always is.
    Part of L814-007.
    
    
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4072 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/MixedConstructs/src/slists.adb
+++ testsuite/Qualif/Ada/stmt/MixedConstructs/src/slists.adb
@@ -1,11 +1,18 @@
 with Vbufs; use Vbufs;
+with Simple_Pools;
 
 package body Slists is
-
+   
+   package Sensor_Nodes_Pool is 
+      new Simple_Pools.Basic_Pool (Data_Type => Sensor_Node,
+                                   Data_Access => Sensor_Node_Access,
+                                   Capacity => 20);
+   
    procedure Prepend (S : Sensor_Access; SL : in out Sensor_List) is
-      NA : Sensor_Node_Access;
+      NA : constant Sensor_Node_Access  := Sensor_Nodes_Pool.Allocate;
    begin
-      NA := new Sensor_Node'(S => S, Next => SL.Head);
+      NA.S := S;
+      NA.Next := SL.Head;
       SL.Head := NA;
       SL.Len := SL.Len + 1;
    end;
