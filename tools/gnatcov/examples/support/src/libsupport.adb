d64eddffebf193a152b5b9f190ddcbdac01474c8
Olivier Hainque <hainque@adacore.com>
2012-09-05 09:31:14 +0000

    * support/src/libsupport.ad?: Provide the low level mem related 	simple services always (memcmp, memcpy, memmove)
    The code for these is simple and target independant here, so not
    a maintenance hazard. Having them here makes sure our testing relies
    on the same code for every configuration, which actually is a nice
    property. Besides, this allows support coverage tests for a wide range
    of constructs that really don't commit RTS violation per se at the
    user level, unlike one using an explicit allocator for example.
    
    Part of L814-007.
    
    
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4075 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ tools/gnatcov/examples/support/src/libsupport.adb
@@ -0,0 +1,90 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                              GNATcoverage                                --
+--                                                                          --
+--                       Copyright (C) 2012, AdaCore                        --
+--                                                                          --
+-- GNATcoverage is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Unchecked_Conversion;
+
+package body Libsupport is
+
+   subtype mem is char_array (size_t);
+   type memptr is access all mem;
+   function to_memptr is new Ada.Unchecked_Conversion (Address, memptr);
+
+   ------------
+   -- memcmp --
+   ------------
+
+   function memcmp (S1 : Address; S2 : Address; N : size_t) return int is
+      s1_p : constant memptr := to_memptr (S1);
+      s2_p : constant memptr := to_memptr (S2);
+   begin
+      for J in 0 .. N - 1 loop
+         if s1_p (J) < s2_p (J) then
+            return -1;
+         elsif s1_p (J) > s2_p (J) then
+            return 1;
+         end if;
+      end loop;
+      return 0;
+   end memcmp;
+
+   ------------
+   -- memcpy --
+   ------------
+
+   procedure memcpy (Dest : Address; Src : Address; N : size_t) is
+      dest_p : constant memptr := to_memptr (Dest);
+      src_p  : constant memptr := to_memptr (Src);
+   begin
+      if N > 0 then
+         for J in 0 .. N - 1 loop
+            dest_p (J) := src_p (J);
+         end loop;
+      end if;
+   end memcpy;
+
+   -------------
+   -- memmove --
+   -------------
+
+   procedure memmove (Dest : Address; Src : Address; N : size_t) is
+      dest_p : constant memptr := to_memptr (Dest);
+      src_p  : constant memptr := to_memptr (Src);
+   begin
+      --  Return immediately if no bytes to copy.
+
+      if N = 0 then
+         return;
+      end if;
+
+      --  This function must handle overlapping memory regions
+      --  for the source and destination. If the Dest buffer is
+      --  located past the Src buffer then we use backward copying,
+      --  and forward copying otherwise.
+
+      if Dest > Src then
+         for J in reverse 0 .. N - 1 loop
+            dest_p (J) := src_p (J);
+         end loop;
+      else
+         for J in 0 .. N - 1 loop
+            dest_p (J) := src_p (J);
+         end loop;
+      end if;
+   end memmove;
+
+end Libsupport;
--- tools/gnatcov/examples/support/src/libsupport.ads
+++ tools/gnatcov/examples/support/src/libsupport.ads
@@ -4,23 +4,44 @@
 --                                                                          --
 --                       Copyright (C) 2012, AdaCore                        --
 --                                                                          --
--- Couverture is free software; you can redistribute it  and/or modify it   --
--- under terms of the GNU General Public License as published by the Free   --
--- Software Foundation; either version 2, or (at your option) any later     --
--- version.  Couverture is distributed in the hope that it will be useful,  --
--- but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-  --
+-- GNATcoverage is free software; you can redistribute it and/or modify it  --
+-- under terms of the GNU General Public License as published by the  Free  --
+-- Software  Foundation;  either version 3,  or (at your option) any later  --
+-- version. This software is distributed in the hope that it will be useful --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
 -- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
--- License  for more details. You  should  have  received a copy of the GNU --
--- General Public License  distributed with GNAT; see file COPYING. If not, --
--- write  to  the Free  Software  Foundation,  59 Temple Place - Suite 330, --
--- Boston, MA 02111-1307, USA.                                              --
---                                                                          --
+-- License for  more details.  You should have  received  a copy of the GNU --
+-- General  Public  License  distributed  with  this  software;   see  file --
+-- COPYING3.  If not, go to http://www.gnu.org/licenses for a complete copy --
+-- of the license.                                                          --
 ------------------------------------------------------------------------------
 
+with System; use System;
+with Interfaces.C; use Interfaces.C;
+
 package Libsupport is
-   --  Dummy root package for our testsuite support library, making sure
-   --  it has at least one source file around.  The actual contents depends
-   --  on the target (to fill gaps in the available RTS) and the kind of
-   --  build attempted (e.g. without even a local last_chance_handler for
-   --  Aunit).
+   --  Root package for our testsuite support library, aimed at filling
+   --  RTS gaps.
+
+   --  The common part here provides the really basic common ground that we
+   --  want to be able to rely on for any possible kind of RTS profile, down
+   --  to strict ZFP.
+
+   --  Simple services possibly invoked by the low level code generation
+   --  passes, without Ada RTS violation per se at the user level (e.g. when
+   --  doing array or slice assignments or comparisons).
+
+   function memcmp (S1 : Address; S2 : Address; N : size_t) return int;
+   pragma Export (C, memcmp, "memcmp");
+
+   procedure memcpy (Dest : Address; Src : Address; N : size_t);
+   pragma Export (C, memcpy, "memcpy");
+
+   procedure memmove (Dest : Address; Src : Address; N : size_t);
+   pragma Export (C, memmove, "memmove");
+
+   --  Other contents depends on the target (to fill gaps in the available
+   --  RTS) and the kind of build we're doing (e.g. without even a local
+   --  last_chance_handler for Aunit).
+
 end Libsupport;
