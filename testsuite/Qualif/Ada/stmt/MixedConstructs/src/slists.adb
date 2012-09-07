fe4be29c301a89448695afc39a93826e887064e7
Olivier Hainque <hainque@adacore.com>
2012-09-07 09:50:01 +0000

    Qualif/Ada 	* stmt/MixedConstructs/src/slists.adb: Adjust wrt recent 	changes in the Simple_Pools interface.
    Ongoing work for L814-007
    
    
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4078 020d506d-db78-4e55-b2a8-6c851f84c332

--- testsuite/Qualif/Ada/stmt/MixedConstructs/src/slists.adb
+++ testsuite/Qualif/Ada/stmt/MixedConstructs/src/slists.adb
@@ -3,18 +3,14 @@ with Simple_Pools;
 
 package body Slists is
    
-   package Sensor_Nodes_Pool is 
-      new Simple_Pools.Basic_Pool (Data_Type => Sensor_Node,
-                                   Data_Access => Sensor_Node_Access,
-                                   Capacity => 20);
+   package Sensor_Nodes_Pool is new
+     Simple_Pools.Basic_Pool (Data_Type => Sensor_Node, Capacity => 20);
    
    procedure Prepend (S : Sensor_Access; SL : in out Sensor_List) is
-      NA : constant Sensor_Node_Access  := Sensor_Nodes_Pool.Allocate;
+      NA : constant Sensor_Nodes_Pool.Data_Access := Sensor_Nodes_Pool.Allocate;
    begin
-      NA.S := S;
-      NA.Next := SL.Head;
-      SL.Head := NA;
-      SL.Len := SL.Len + 1;
+      NA.all := (S => S, Next => SL.Head);
+      SL := (Head => Sensor_Node_Access (NA), Len => SL.Len + 1);
    end;
 
 end;
