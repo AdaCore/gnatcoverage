0d667e5f7ad27846777e3ecf5f992fbd1ed0d6bb
Pierre-Marie de Rodat <derodat@adacore.com>
2013-03-08 09:54:12 +0000

    Put the architecture discovery logic in architecture.mk
    M219-028
    
    gnatcov's Makefile ARCH variable is renammed BITS_ARCH and is also deduced
    from `uname -m`.
    
    git-svn-id: svn+ssh://scm.forge.open-do.org/scmrepos/svn/couverture/trunk/couverture@4389 020d506d-db78-4e55-b2a8-6c851f84c332

---
+++ tools/gnatcov/architecture.mk
@@ -0,0 +1,25 @@
+DISTRIB_ARCH=$(shell uname -m)
+
+#
+# Translation from `uname -m` to a suitable value for valgrind
+#
+VALGRIND_ARCH_i386   = x86
+VALGRIND_ARCH_i486   = x86
+VALGRIND_ARCH_i586   = x86
+VALGRIND_ARCH_i686   = x86
+VALGRIND_ARCH_x86_64 = amd64
+VALGRIND_ARCH_amd64  = amd64
+
+VALGRIND_ARCH=$(VALGRIND_ARCH_$(DISTRIB_ARCH))
+
+#
+# Translation from `uname -m` to 32 or 64 (bits)
+#
+BITS_ARCH_i386   = 32
+BITS_ARCH_i486   = 32
+BITS_ARCH_i586   = 32
+BITS_ARCH_i686   = 32
+BITS_ARCH_x86_64 = 64
+BITS_ARCH_amd64  = 64
+
+BITS_ARCH=$(BITS_ARCH_$(DISTRIB_ARCH))
