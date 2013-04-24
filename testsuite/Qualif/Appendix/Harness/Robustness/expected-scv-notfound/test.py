0e2f104c7cff5b30861d956ca861f3c039dfe070
Olivier Hainque <hainque@adacore.com>
2013-04-24 15:30:42 +0200

    Fix gnatcov Harness tests not to expect diagnostics on annotated source
    The annotated source outputs are not qualified
    hence not even produced in qualif runs.
    
    Preliminary work for M420-001.

--- testsuite/Qualif/Appendix/Harness/Robustness/expected-scv-notfound/test.py
+++ testsuite/Qualif/Appendix/Harness/Robustness/expected-scv-notfound/test.py
@@ -1,15 +1,22 @@
 from SCOV.htc import *
 from SCOV.tctl import CAT
 
+# The =report diags we always expect. The =xcov diags we only expect in
+# !qualif runs as these don't produce the =xcov outputs.
+
+expected_diags = [
+    HarnessDiagnostic (
+        text = "Missing expected sNoCov mark at line 3")
+    ]
+
+if not thistest.options.qualif_level:
+    expected_diags.append (
+        HarnessDiagnostic (
+            text = "Missing expected sNoCov mark at line 3")
+        )
+
 HarnessTestCase(
-    expected_diags = [
-        HarnessDiagnostic (
-            text = "Missing expected sNoCov mark at line 3",
-            ),
-        HarnessDiagnostic (
-            text = "(inc.adb.xcov) Missing expected lNoCov mark at line 3",
-            )
-        ],
+    expected_diags=expected_diags,
     category=CAT.stmt).run()
 
 thistest.result()
