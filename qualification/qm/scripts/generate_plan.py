c4fa7b65901d9fc3c2c342f8cf597e6761a5cc6f
Olivier Hainque <hainque@adacore.com>
2013-04-26 18:10:54 +0200

    Split the qm generation scripts for plans by output format.
    So the toplevel driver can generate just the one it needs.
    
    Also, pick an output dir name consistent with what other sphinx-based
    components do, simplifying overall processing across the different
    parts of the qualification material.
    
    Ongoing work for M420-001

--- qualification/qm/scripts/generate_plan.py
+++
@@ -1,4 +0,0 @@
-import generate_doc
-
-generate_doc.generate_pdf('plans', 'release/pdf')
-generate_doc.generate_html('plans', 'release/plans_html')
---
+++ qualification/qm/scripts/generate_plans_pdf.py
@@ -0,0 +1,7 @@
+import generate_doc
+
+# Locate the output as the other sphinx-based components do, facilitating the
+# toplevel driver for qmat generation. Not an unreasonable name anyway.
+
+generate_doc.generate_pdf('plans', 'build/latexpdf')
+
