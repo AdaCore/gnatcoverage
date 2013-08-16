cb547a0f0e164d9ac99a989d7af043246da7d7bb
Pierre-Marie de Rodat <derodat@adacore.com>
2013-08-16 19:21:28 +0200

    disa_x86: fix XMM register numbers in the registers image routine
    Fix for M219-028
    
    Change-Id: Ib315dd2424dfa0000ae2743a2ea09e57b0eb2556

--- tools/gnatcov/disa_x86.adb
+++ tools/gnatcov/disa_x86.adb
@@ -2038,8 +2038,8 @@ package body Disa_X86 is
          Regs_XMM : constant Reg_Name5_Array :=
            ("xmm0 ", "xmm1 ", "xmm2 ", "xmm3 ", "xmm4 ", "xmm5 ", "xmm6 ",
             "xmm7 ",
-            "xmm8 ", "xmm9 ", "xmm10", "xmm12", "xmm13", "xmm14", "xmm15",
-            "xmm16");
+            "xmm8 ", "xmm9 ", "xmm10", "xmm11", "xmm12", "xmm13", "xmm14",
+            "xmm15");
       begin
          Add_Char ('%');
          case R is
