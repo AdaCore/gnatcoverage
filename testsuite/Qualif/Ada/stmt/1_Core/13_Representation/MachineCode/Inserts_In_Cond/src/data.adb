211bedc13712de60c2187498068525340f33684e
Olivier Hainque <hainque@adacore.com>
2013-11-15 19:37:23 +0100

    gnatcov qualkit: use nop instead of comment in MachineCode inserts
    Testing processing of Asm inserts for stmt/chap13. nop is
    common enough for our current set of targets and generates
    a machine insn, more in line with the test purpose.
    
    Followup on MA27-002

--- testsuite/Qualif/Ada/stmt/1_Core/13_Representation/MachineCode/Inserts_In_Cond/src/data.adb
+++ testsuite/Qualif/Ada/stmt/1_Core/13_Representation/MachineCode/Inserts_In_Cond/src/data.adb
@@ -1,18 +1,15 @@
 with System.Machine_Code; use System.Machine_Code;
 
 package body Data is
+   
    function Id (X, Pathno: Integer) return Integer is
       Copy : Integer := X;
       pragma Volatile (Copy);      
    begin
       if Pathno = 1 then
-         Asm ("# stw %1, %0", Volatile => True,  -- # asm_1
-              Outputs => Integer'Asm_Output ("=m", Copy), -- # extra_1
-              Inputs => Integer'Asm_Input ("r", X));     -- # extra_1
+         Asm ("nop", Volatile => True);  -- # asm_1
       else
-         Asm ("# stw %1, %0",  Volatile => True, -- # asm_n
-              Outputs => Integer'Asm_Output ("=m", Copy),  -- # extra_n
-              Inputs => Integer'Asm_Input ("r", X));      -- # extra_n
+         Asm ("nop", Volatile => True);  -- # asm_n
       end if;
       return Copy;
    end;
