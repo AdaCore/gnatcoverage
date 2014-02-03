4cc8995f0506962b5cc6a7b1af2499c70cf1a505
Olivier Hainque <hainque@adacore.com>
2014-02-03 12:49:57 +0100

    gnatcov TOR: add TC for membership tests and LRMREFs for 4.5.2
    MB23-001

---
+++ testsuite/Qualif/Ada/decision/1_Core/Operands/RangeMembership/src/fuor.ads
@@ -0,0 +1,14 @@
+package FUOR is
+   
+   function Eval (R1, R2 : Integer) return Boolean;
+
+   -- Whether R1+R2 is in 1 .. 10 or else R1+R2 is in 20 .. 30
+
+
+   R1 : Integer := 7;
+
+   R2_FF : Integer := 8; 
+   R2_FT : Integer := 15; 
+
+   R2_TX : Integer := 2; 
+end;
