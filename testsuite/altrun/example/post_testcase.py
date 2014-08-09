5c4dce31e7f107d8bba77efe57a2d43aeedcbe28
Olivier Hainque <hainque@adacore.com>
2014-08-09 15:12:57 +0200

    gnatcov suite: add an example post-testcase hook in altrun/example
    Continuation of N819-016

---
+++ testsuite/altrun/example/post_testcase.py
@@ -0,0 +1,7 @@
+# This python script is invoked after each testcase has finished
+# running.
+
+import os
+
+print "Post Testcase example hook running !!"
+print "Current directory is " + os.getcwd()
