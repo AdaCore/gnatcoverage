ad61441be583ff5116b806fe1565934acea06618
Olivier Hainque <hainque@adacore.com>
2014-10-28 16:39:40 +0100

    gnatcov suite: add missing expectations on bare decls in MultiStmtLines case
    L619-049

--- testsuite/Qualif/Ada/decision/Robustness/MultiStmtLines/Checks/src/test_register1.adb
+++ testsuite/Qualif/Ada/decision/Robustness/MultiStmtLines/Checks/src/test_register1.adb
@@ -15,6 +15,7 @@ end;
 --  /double/ l! ## s-:"This_Score .=",dT-:"if Double"
 --  /triple/ l! ## s-:"This_Score .=",dT-:"if Triple"
 --  /hits/   l! ## s-:"G.Fancy_Hits .=",dT-
+--  /decl/  ~l- ## ~s-
 --  /times/  l- ## s-
   
 --  7.0.3 is imprecise with multiple stmts on a line
