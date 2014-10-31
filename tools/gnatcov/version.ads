133b40c61c9603e04a6e0daa15e2d181cbd3465a
Olivier Hainque <hainque@adacore.com>
2014-10-31 10:30:45 +0100

    Set the gnatcov version number on the 1.3.1 branch
    NA21-013

--- tools/gnatcov/version.ads
+++ tools/gnatcov/version.ads
@@ -2,7 +2,7 @@
 --                                                                          --
 --                               GNATcoverage                               --
 --                                                                          --
---                     Copyright (C) 2006-2013, AdaCore                     --
+--                     Copyright (C) 2006-2014, AdaCore                     --
 --                                                                          --
 -- GNATcoverage is free software; you can redistribute it and/or modify it  --
 -- under terms of the GNU General Public License as published by the  Free  --
@@ -17,5 +17,5 @@
 ------------------------------------------------------------------------------
 
 package Version is
-   Xcov_Version : constant String := "1.3.0w (20000000)";
+   Xcov_Version : constant String := "1.3.1 (20141030)";
 end Version;
