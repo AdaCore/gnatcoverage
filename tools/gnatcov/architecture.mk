67b843c71f833fb52407bbd5d9d370c2ea75957a
Pierre-Marie de Rodat <derodat@adacore.com>
2015-06-04 16:33:51 +0200

    architecture.mk: match the "x86" platform name
    Fix for O603-050

--- tools/gnatcov/architecture.mk
+++ tools/gnatcov/architecture.mk
@@ -3,6 +3,7 @@ DISTRIB_ARCH=$(shell uname -m)
 #
 # Translation from `uname -m` to a suitable value for valgrind
 #
+VALGRIND_ARCH_x86    = x86
 VALGRIND_ARCH_i386   = x86
 VALGRIND_ARCH_i486   = x86
 VALGRIND_ARCH_i586   = x86
@@ -15,6 +16,7 @@ VALGRIND_ARCH=$(VALGRIND_ARCH_$(DISTRIB_ARCH))
 #
 # Translation from `uname -m` to 32 or 64 (bits)
 #
+BITS_ARCH_x86    = 32
 BITS_ARCH_i386   = 32
 BITS_ARCH_i486   = 32
 BITS_ARCH_i586   = 32
