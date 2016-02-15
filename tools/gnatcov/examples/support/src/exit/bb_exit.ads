37faf842feccf3811575f5b6d6be1241da52dedb
Olivier Hainque <hainque@adacore.com>
2016-02-15 14:27:36 +0100

    gnatcov suite: provide an "exit" API from libsupport
    To support our silent_last_chance handler in configurations
    that don't provide the standard API.
    
    Part of P208-027 and P210-034
    
    Change-Id: I86027236c35276066ed380ae721cdb3ffd31b5ca

---
+++ tools/gnatcov/examples/support/src/exit/bb_exit.ads
@@ -0,0 +1,26 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                              GNATcoverage                                --
+--                                                                          --
+--                     Copyright (C) 2008-2016, AdaCore                     --
+--                                                                          --
+-- Couverture is free software; you can redistribute it  and/or modify it   --
+-- under terms of the GNU General Public License as published by the Free   --
+-- Software Foundation; either version 2, or (at your option) any later     --
+-- version.  Couverture is distributed in the hope that it will be useful,  --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-  --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License  for more details. You  should  have  received a copy of the GNU --
+-- General Public License  distributed with GNAT; see file COPYING. If not, --
+-- write  to  the Free  Software  Foundation,  59 Temple Place - Suite 330, --
+-- Boston, MA 02111-1307, USA.                                              --
+--                                                                          --
+------------------------------------------------------------------------------
+
+package BB_Exit is
+
+   procedure Program_Exit (Code : Integer);
+   pragma Export (C, Program_Exit, "exit");
+   pragma No_Return (Program_Exit);
+   
+end BB_Exit;
