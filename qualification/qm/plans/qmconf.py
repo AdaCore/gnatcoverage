b72ea24782a5215a40b27b781702ba6d456ef01d
Olivier Hainque <hainque@adacore.com>
2016-10-06 19:00:53 +0200

    gnatcov qualkit: introduce common qm/rest configuration file
    To provide common macros for use in the various documents.
    
    P914-022
    
    Change-Id: I2f1162545ae12d246582d61e69c26e5576a24fb5

--- qualification/qm/plans/qmconf.py
+++ qualification/qm/plans/qmconf.py
@@ -1,5 +1,21 @@
 version='dev'
-author='AdaCore'
-copyright='AdaCore, 2013'
-project_name='GNATcoverage'
 
+# This file gets copied by sphinx as something like:
+#    .../qualification/qm/.qm/PLANS/_sphinx/conf.py
+#
+# and the common configuration file is in qualification/qm ...
+
+common_file = os.path.join(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.abspath(__file__))))),
+    "common_conf.py")
+
+if os.path.isfile(common_file):
+    execfile(common_file)
+else:
+    print "Couldn't find common configuration file"
+    print common_file
+    print "from: %s" % __file__
