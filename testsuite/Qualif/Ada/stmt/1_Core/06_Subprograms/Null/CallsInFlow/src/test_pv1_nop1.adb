5710d1fac0f1c6a606e681414cffdd3c8c86cfbe
Olivier Hainque <hainque@adacore.com>
2016-10-11 12:26:26 +0200

    gnatcov Qualif: Add test checking diagnostics on calls to null procedures
    P911-005 and PA09-008
    
    Change-Id: I20bd8b029c476486ba1769370a3a355305ccca1b

---
+++ testsuite/Qualif/Ada/stmt/1_Core/06_Subprograms/Null/CallsInFlow/src/test_pv1_nop1.adb
@@ -0,0 +1,42 @@
+with Support, Ops; use Support, Ops;
+
+procedure Test_PV1_NOP1 is
+   X : Integer := 21;
+begin
+   Process (X, Op_Nop1, PV1);
+   Assert (X = 21);
+end;
+
+--# ops.adb
+--  /stmt/ l+ ## 0
+--  /pv1_test_nop1/ l+ ## 0
+--  /pv1_nop1/ l+ ## 0
+--  /pv1_test_nop2/ l- ## s-
+--  /pv1_nop2/ l- ## s-
+--  /pv1_test_inc/  l- ## s-
+--  /pv1_inc/ l- ## s-
+--  /pv1_test_nop3/ l- ## s-
+--  /pv1_nop3/ l- ## s-
+
+--  /pv2_test/ l- ## s-
+--  /pv2_nop1/ l- ## s-
+--  /pv2_nop2/ l- ## s-
+--  /pv2_inc/  l- ## s-
+--  /pv2_nop3/ l- ## s-
+
+--  /pv1_call/ l+ ## 0
+--  /pv2_call/ l- ## s-
+
+--  /do_inc/ l- ## s-
+--  /do_nop2/ l- ## s-
+--  /do_nop3/ l- ## s-
+
+--  %cargs: -O1
+--  =/do_nop3/ ~l- ## ~s-
+
+--# ops.ads
+--  /do_nop1/ l+ ## 0
+
+--  %cargs: -O1
+--  =/do_nop1/ ~l+ ## 0
+
---
+++ testsuite/Qualif/Ada/stmt/1_Core/06_Subprograms/Null/CallsInFlow/src/test_pv1_nop2.adb
@@ -0,0 +1,42 @@
+with Support, Ops; use Support, Ops;
+
+procedure Test_PV1_NOP2 is
+   X : Integer := 21;
+begin
+   Process (X, Op_Nop2, PV1);
+   Assert (X = 21);
+end;
+
+--# ops.adb
+--  /stmt/ l+ ## 0
+--  /pv1_test_nop1/ l+ ## 0
+--  /pv1_nop1/ l- ## s-
+--  /pv1_test_nop2/ l+ ## 0
+--  /pv1_nop2/ l+ ## 0
+--  /pv1_test_inc/  l- ## s-
+--  /pv1_inc/ l- ## s-
+--  /pv1_test_nop3/ l- ## s-
+--  /pv1_nop3/ l- ## s-
+
+--  /pv2_test/ l- ## s-
+--  /pv2_nop1/ l- ## s-
+--  /pv2_nop2/ l- ## s-
+--  /pv2_inc/  l- ## s-
+--  /pv2_nop3/ l- ## s-
+
+--  /pv1_call/ l+ ## 0
+--  /pv2_call/ l- ## s-
+
+--  /do_inc/ l- ## s-
+--  /do_nop2/ l+ ## 0
+--  /do_nop3/ l- ## s-
+
+--  %cargs: -O1
+--  =/do_nop3/ ~l- ## ~s-
+
+--# ops.ads
+--  /do_nop1/ l- ## s-
+
+--  %cargs: -O1
+--  =/do_nop1/ ~l- ## ~s-
+
