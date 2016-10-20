6c3120767ee24a157ac10c0390e1bd26c9400cfa
Olivier Hainque <hainque@adacore.com>
2016-10-20 11:53:06 +0200

    gnatcov TOR: get common_conf.py
    To allow references to standard document names with macros.
    
    PA06-006 (qualkit for x86-windows/native Ada2005 doA)

--- testsuite/Qualif/qmconf.py
+++ testsuite/Qualif/qmconf.py
@@ -1,10 +1,25 @@
 author='AdaCore'
 version='dev'
-copyright='AdaCore, 2013'
+copyright='AdaCore, 2016'
 project_name='GNATCoverage'
 latex_show_pagerefs=True
 
-rst_prolog = writer.define_role('raw-html(raw)', {'format': 'html'})
+common_file = os.path.join(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.abspath(__file__))))),
+    "common_conf.py")
+
+if os.path.isfile(common_file):
+    execfile(common_file)
+else:
+    print "Couldn't find common configuration file"
+    print common_file
+    print "from: %s" % __file__
+
+rst_prolog += writer.define_role('raw-html(raw)', {'format': 'html'})
 
 html_sidebars = {
    '**': ['localtoc.html', 'sourcelink.html', 'searchbox.html']
