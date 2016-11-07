aff049d21a9aa3cde3859b65e97713671ef3974f
Olivier Hainque <hainque@adacore.com>
2016-11-07 14:22:21 +0100

    gnatcov qualkit: adjust common_conf to support doc identifiers
    Remove definition of "author", add "users" and "assignments" so
    filling a doc_id in qmconf.py will have the qm produce document
    identification on every page.
    
    Part of PB06-002 (straighten gnatcov qualkit doc traceability)

--- qualification/qm/common_conf.py
+++ qualification/qm/common_conf.py
@@ -3,7 +3,27 @@ from datetime import date
 project_name = 'GNATcoverage'
 project_name_it = '*%s*'% project_name
 owner = 'AdaCore'
-author = 'AdaCore'
+
+users = {
+    'hainque': {'name': 'Olivier Hainque',
+                'company': owner,
+                'email': 'hainque@adacore.com',
+                'team': 'Qualification'},
+
+    'guitton': {'name': 'Jerome Guitton',
+                'company': owner,
+                'email': 'guitton@adacore.com',
+                'team': 'Qualification'},
+
+    'fofanov': {'name': 'Vasily Fofanov',
+                'company': owner,
+                'email': 'fofanov@adacore.com',
+                'team': 'Qualification'}}
+
+assignments = {
+    'authors'   : ['hainque'],
+    'reviewers' : ['guitton', 'fofanov'],
+    'approvers' : ['fofanov']}
 
 release='DRAFT 0.0'
 version='(%s)' % release
