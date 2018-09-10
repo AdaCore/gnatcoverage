40ea712648a822caf3d25b96516b9121eedf5bea
Fabien Chouteau <chouteau@adacore.com>
2018-09-10 17:49:09 +0200

    altrun/trace32: Use t32usbchecker to reset the probe
    This can be needed when the testsuite crashes, for instance with an
    rlimit. The probe is still trying to talk with the previous Trace32
    instance that was killed by rlimit.
    
    It should be possble to achive the same result with the
    CONNECTIONMODE=AUTOCONNECT parameter of the configuration, however this
    parameter doesn't work with the SCREEN=OFF mode.
    
    Part of O721-022.
    
    Change-Id: Id44d5dcf38ee906da34b4ffa9b9b7bc5e03c8a54

--- testsuite/altrun/trace32/pre_testsuite.py
+++ testsuite/altrun/trace32/pre_testsuite.py
@@ -18,12 +18,24 @@ putenv('T32SYS', PATH_TO_T32_HOME)
 putenv('T32TMP', "/tmp")
 putenv('T32PDFVIEWER', PATH_TO_T32_HOME + "/bin/pc_linux64/t32_startpdfviewer.sh")
 
-ofile = "cmd_.out"
+# Run t32usbchecker to reset the probe in case it is already connected.
+#
+# This can happen when the testsuite crashes, for instance with an rlimit. The
+# probe is still trying to talk with the previous Trace32 instance that was
+# killed by rlimit.
+#
+# It should be possble to achive the same result with the
+# CONNECTIONMODE=AUTOCONNECT parameter of the configuration, however this
+# parameter doesn't work with the SCREEN=OFF mode.
+p = gnatpython.ex.Run([os.path.dirname(PATH_TO_T32) + '/t32usbchecker'],
+                      output="t32usbchecker.out",
+                      bg=False)
+
 p = gnatpython.ex.Run([PATH_TO_T32,
                        "-c",
                        os.path.join(altrun_dir_path, 'config.t32')
                        ],
-                      output=ofile,
+                      output="t32.out",
                       bg=True)
 
 sleep(2)
