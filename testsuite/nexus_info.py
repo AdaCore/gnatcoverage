f2ff6af03f8e2f5212c921013ea575ef2ad05bdc
Pierre-Marie de Rodat <derodat@adacore.com>
2019-03-26 10:19:57 +0100

    nexus_info.py: fix flake8 diagnostics
    Change-Id: I758d9b4151a7fa80acdfbbfd8d05380be08e46f3
    TN: S326-007

--- testsuite/nexus_info.py
+++ testsuite/nexus_info.py
@@ -5,24 +5,30 @@ cmgr = ic.ConnectionMgr()
 cmgr.connectMRU('')
 ideController = ic.CIDEController(cmgr)
 
+
 def winIDEA_version():
     return ideController.getWinIDEAVersion().toString()
 
+
 def firmware_revision():
     firmwareDateInstalledUrl = '/iOPEN/SystemConfig.Firmware.DateInstalled'
-    firmwareDateInstalled = ideController.getOptionInt(firmwareDateInstalledUrl)
+    firmwareDateInstalled = ideController.getOptionInt(
+        firmwareDateInstalledUrl)
     year = firmwareDateInstalled >> 16
     month = (firmwareDateInstalled & 0xFF00) >> 8
     date = (firmwareDateInstalled & 0xFF)
 
     return '{}{:02d}{:02d}'.format(year, month, date)
 
+
 def probe_model():
     # No way to retrieve this automatically. This is what we have:
     return 'iSystem iC5000'
 
+
 def workspace_file():
-    return ' '.join(fu.find (root="altrun", pattern="justrun.xjrf"))
+    return ' '.join(fu.find(root="altrun", pattern="justrun.xjrf"))
+
 
 print (
     "Nexus Framework ## "
