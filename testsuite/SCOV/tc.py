5408169da12627db6517e6e7566dc2e22a50593a
Olivier Hainque <hainque@adacore.com>
2019-03-29 15:19:53 +0100

    gnatcov testsuite: move the CovControl class
    From SCOV.tc to SCOV.tctl, where it belongs pretty naturally
    and from where it can be imported by SCOV.internals.drivers
    without introducing a cycle in the import graph.
    
    The import of tctcl.CovControl from tc only allows former
    tests to keep working. tc doesn't use CovControl itself.
    
    Change-Id: I4d39e3afddc835822f9c1109edc4bf6444a2a836
    TN: S321-037

--- testsuite/SCOV/tc.py
+++ testsuite/SCOV/tc.py
@@ -33,123 +33,13 @@ from SUITE.cutils import to_list, contents_of, FatalError
 from SUITE.tutils import TEST_DIR
 
 from SUITE.qdata import Qdata, QDentry
-from SUITE.gprutils import gprcov_for
 
 from gnatpython.fileutils import ls
 
 from internals.driver import SCOV_helper_bin_traces, SCOV_helper_src_traces
 from internals.driver import WdirControl
 
-from SCOV.tctl import CAT
-
-# =================================
-# == User Level Coverage Control ==
-# =================================
-
-# Facility to allow GPR level control and general options control for
-# "gnatcov coverage". An instance of this might be passed to the
-# TestCase run() invocation below, to
-#
-#   - Influence the project file constructed to build the testcase binaries,
-#     with dependencies to other projects or directives pertaining to the
-#     Coverage package within the project (deps).
-#
-#   - State the set of units for which we expect reports as result. The
-#     testsuite driver will then check that we do get the expected reports and
-#     only those. For those that are expected, the testsuite performs the
-#     standard checks of matching outcome with expectations stated in drivers
-#     (units_in, units_out, ulist_in, ulist_out).
-#
-#   - Influence the GPR related command line options to gnatcov, controlling
-#     which projects are to be considered part of the analysis closure
-#     (scooptions).
-#
-#   - Influence the non-GPR related command line options to gnatcov coverage
-#     (covoptions).
-
-class CovControl:
-
-    def __init__ (
-        self, deps = (), units_in = None, ulist_in = None,
-        units_out = None, ulist_out = None, xreports = None,
-        scoptions = "", covoptions=""):
-
-        # To control "with" dependencies (set of projects that will be withed
-        # by the one we will be generating for the testcase):
-
-        self.deps = list(deps)
-
-        # gnatcov options to pass SCOs. As we're setting up a coverage control
-        # object, this is NOT intended for --scos. This is intended for
-        # "--recursive" or "--units" in addition to a -P argument, which needs
-        # to be provided by the caller as well. This will be passed to both
-        # gnatcov run and gnatcov coverage invocations.
-
-        self.scoptions = scoptions
-
-        # Extra options to gnatcov coverage only, typically for "-S" variants.
-
-        self.covoptions = covoptions
-
-        # To control Units related attributes in the GPR Coverage package:
-
-        self.units_in = units_in
-        self.units_out = units_out
-        self.ulist_in = ulist_in
-        self.ulist_out = ulist_out
-
-        # To instruct the testsuite driver about the set of source reports we
-        # expect.
-
-        # None is taken to mean "we don't expect anything in particular" as in
-        # "we don't care", so anything is expected. An empty list is taken to
-        # mean "we expect no report at all", so anything that comes out is
-        # unexpected.
-
-        self.xreports = xreports
-
-    # ------------------
-    # -- [un]expected --
-    # ------------------
-
-    def unexpected (self, source):
-        return (
-            self.xreports is not None
-            and source not in self.xreports)
-
-    def expected (self, source):
-        return not self.unexpected (source)
-
-    # ------------------
-    # -- requires_gpr --
-    # ------------------
-
-    def requires_gpr (self):
-        """Whether honoring this control object implictly requires the use of
-        a project file."""
-
-        return (self.deps
-                or self.scoptions
-                or self.units_in
-                or self.units_out
-                or self.ulist_in
-                or self.ulist_out)
-
-    # ---------
-    # -- gpr --
-    # ---------
-
-    def gpr (self):
-
-        """The GPR Coverage package corresponding to our lists of attribute
-           contents, as a multiline string."""
-
-        return gprcov_for (
-            units_in = self.units_in,
-            units_out = self.units_out,
-            ulist_in = self.ulist_in,
-            ulist_out = self.ulist_out
-            )
+from SCOV.tctl import CAT, CovControl
 
 # ==============
 # == TestCase ==
