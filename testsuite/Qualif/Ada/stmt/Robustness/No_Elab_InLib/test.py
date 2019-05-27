71bb984118de178c3e67c9fe3de6c23e022ed192
Olivier Hainque <hainque@adacore.com>
2019-05-27 09:45:41 +0200

    Qualif: Adjust uses of CovControl to pass GPRswitches
    Part of S523-037
    
    Change-Id: I8a46d96547558ef896f2956cadf838fc0aa11bb7

--- testsuite/Qualif/Ada/stmt/Robustness/No_Elab_InLib/test.py
+++ testsuite/Qualif/Ada/stmt/Robustness/No_Elab_InLib/test.py
@@ -3,8 +3,10 @@ import os.path
 from SCOV.tc import TestCase
 from SCOV.tctl import CovControl
 from SUITE.context import thistest
+from SUITE.gprutils import GPRswitches
 
 libdep = os.path.abspath('mylib/mylib.gpr')
-TestCase().run(covcontrol=CovControl(deps=[libdep],
-                                     scoptions='-Pgen --recursive'))
+TestCase().run(covcontrol=CovControl(
+    deps=[libdep],
+    gprsw=GPRswitches(root_project='gen.gpr', recursive=True)))
 thistest.result()
