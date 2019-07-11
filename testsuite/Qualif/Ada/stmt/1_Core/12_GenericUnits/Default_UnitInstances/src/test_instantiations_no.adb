2a734982e18ef7676349def96ce6b386764bd152
Pierre-Marie de Rodat <derodat@adacore.com>
2019-07-11 14:02:26 +0200

    Qualif/Ada/stmt/1_Core/12_GenericUnits/Default_UnitInstances: adjust...
    ... for source trace mode.
    
    Change-Id: I481d4d20d9b6af2c8d47ce6ea5cc9ab52a5eff1b
    TN: S606-029

--- testsuite/Qualif/Ada/stmt/1_Core/12_GenericUnits/Default_UnitInstances/src/test_instantiations_no.adb
+++ testsuite/Qualif/Ada/stmt/1_Core/12_GenericUnits/Default_UnitInstances/src/test_instantiations_no.adb
@@ -12,37 +12,69 @@ begin
 end Test_Instantiations_No;
 
 --# pack.adb
+--
+--%opts: --trace-mode=bin
 -- /decl/         ~l- ## ~s-
 -- /swap/          l- ## s-
 -- /proc/          l- ## s-
-
+--
+--%opts: --trace-mode=src
+-- /new_value_g/   l- ## s-
+-- /decl/          l- ## s-
+-- /swap/          l- ## s-
+-- /proc/          l- ## s-
+--
+--
 --#  stacks_g.ads
+--
+--%opts: --trace-mode=bin
 -- /decl/          l. ## 0
 -- /elab/          l. ## 0
 -- /line-elab/     l. ## 0c
+--
+--%opts: --trace-mode=src
+-- /decl/          l- ## s-
+-- /elab/          l- ## s-
+-- /line-elab/     l- ## 0
 
 -- %tags:(7.0.2|7.2.2|7.4.3)
 -- =/decl/         l- ## s-
 -- =/elab/         l- ## s-
 -- =/line-elab/    l- ## 0c
-
+--
+--
 --#  stacks_g.adb
+--
+--%opts: --trace-mode=bin
 -- /push/          l. ## 0
 -- /pop/           l. ## 0
 -- /n_values/      l. ## 0
 -- /default_stack/ l. ## 0
 -- /elab/          l. ## 0
-
+--
+--%opts: --trace-mode=src
+-- /push/          l- ## s-
+-- /pop/           l- ## s-
+-- /n_values/      l- ## s-
+-- /default_stack/ l- ## s-
+-- /elab/          l- ## s-
+--
 -- %tags:(7.0.2|7.2.2|7.4.3)
-
+--
 -- =/push/          l- ## s-
 -- =/pop/           l- ## s-
 -- =/n_values/      l- ## s-
 -- =/default_stack/ l- ## s-
 -- =/elab/          l- ## s-
-
+--
+--
 --# update_g.adb
+--
+--%opts: --trace-mode=bin
 -- /stmt/          l. ## 0
+
+--%opts: --trace-mode=src
+-- /stmt/          l- ## s-
 
 -- %tags:(7.0.2|7.2.2|7.4.3)
 -- =/stmt/         l- ## s-
--- testsuite/Qualif/Ada/stmt/1_Core/12_GenericUnits/Default_UnitInstances/test.py
+++ testsuite/Qualif/Ada/stmt/1_Core/12_GenericUnits/Default_UnitInstances/test.py
@@ -2,5 +2,11 @@ from SCOV.tc import TestCase
 from SCOV.tctl import CovControl
 from SUITE.context import thistest
 
-TestCase().run(CovControl(covoptions="--source-search=../../src"))
+
+# In binary trace mode, we need to help gnatcov find the source for
+# stacks_g.ads, as no debug info references it (debug info is the only source
+# of absolute paths for source files).
+TestCase().run(
+    CovControl(covoptions="--source-search=../../src")
+    if thistest.options.trace_mode == 'bin' else None)
 thistest.result()
