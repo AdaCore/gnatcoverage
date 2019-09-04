61859ab416e8d12182df1a433035f3aa73cf60f7
Pierre-Marie de Rodat <derodat@adacore.com>
2019-09-04 14:02:27 +0200

    Add a developer convenience script to build and install gnatcov_rts_full
    Change-Id: I43931e5b5c24af7aae2d35bed00bb60dd02cda05
    TN: S819-028

---
+++ tools/gnatcov/rts/install.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# This is a developer convenience script to build and install gnatcov_rts_full
+# in the toolchain's default prefix.
+
+# If gnatcov_rts_full was already installed, just uninstall it (one single
+# gprbuild --uninstall is enough for all variants).
+gprinstall --uninstall -Pgnatcov_rts_full
+
+# Build and install it for all supported library types
+for i in static static-pic relocatable
+do
+    gprbuild   -P gnatcov_rts_full.gpr -XLIBRARY_TYPE=$i -p
+    gprinstall -P gnatcov_rts_full.gpr  -XLIBRARY_TYPE=$i -p \
+        --sources-subdir=include/gnatcov_rts_full \
+        --build-var=LIBRARY_TYPE --build-name=$i
+done
