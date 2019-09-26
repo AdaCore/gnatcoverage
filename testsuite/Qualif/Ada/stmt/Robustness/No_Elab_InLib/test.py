c7784b862a92ee674579dfa77e9f937e08b41f23
Pierre-Marie de Rodat <derodat@adacore.com>
2019-09-26 05:31:54 -0400

    Qualif/Ada/stmt/Robustness/No_Elab_InLib: specify units of interest
    The goal of this testcase is to check the coverage report for specific
    units in a project. Explicitly request these units to avoid future
    warnings from gnatcov about units of interest not having library info
    files.
    
    Change-Id: I9a2f72d18ba508b00cf57705f1a7915192ba6b87
    TN: S918-005

--- testsuite/Qualif/Ada/stmt/Robustness/No_Elab_InLib/test.py
+++ testsuite/Qualif/Ada/stmt/Robustness/No_Elab_InLib/test.py
@@ -8,5 +8,7 @@ from SUITE.gprutils import GPRswitches
 libdep = os.path.abspath('mylib/mylib.gpr')
 TestCase().run(covcontrol=CovControl(
     deps=[libdep],
-    gprsw=GPRswitches(root_project='gen.gpr', recursive=True)))
+    gprsw=GPRswitches(root_project='gen.gpr',
+                      units=['foo', 'bar', 'klunk'],
+                      recursive=True)))
 thistest.result()
