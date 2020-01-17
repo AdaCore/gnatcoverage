ddd0269921d4e58d96b321a92faec2a3d09d43c0
Thomas Quinot <quinot@adacore.com>
2020-01-17 18:27:43 +0100

    Avoid triggering compiler bug in instrumented code
    In some cases, the code generated for MC/DC instrumentation of
    expression functions (S725-026) triggers a compiler bug (T114-003).
    Avoid the problematic pattern in unrelated tests within the
    qualification test suite.
    
    Change-Id: I19c2d84d828e74624f5891677f43d74822f1ff6d

--- testsuite/Qualif/Ada/mcdc/1_Core/Operands/Uindexed/src/fuand.ads
+++ testsuite/Qualif/Ada/mcdc/1_Core/Operands/Uindexed/src/fuand.ads
@@ -4,21 +4,22 @@ pragma Ada_2012;
 -- yielding Integer values.
 
 package FUAND is
-   
-   type Op_Name is (A, B);   
+
+   type Op_Name is (A, B);
    type Varray is array (Op_Name) of aliased Integer;
-   
+
    type Operands is tagged record
       Values : Varray;
    end record
      with Variable_Indexing => V_Indexing;
-   
+
    type Integer_Ref (Ref : access Integer) is
      null record with Implicit_Dereference => Ref;
-  
+
+   function Andthen (Ops : Operands) return Boolean;
+
    function V_Indexing
      (X : aliased in out Operands; Op : Op_Name)
      return Integer_Ref is (Ref => X.Values (Op)'Access);
 
-   function Andthen (Ops : Operands) return Boolean;
 end;
--- testsuite/Qualif/Ada/mcdc/1_Core/Operands/Uindexed/src/fuor.ads
+++ testsuite/Qualif/Ada/mcdc/1_Core/Operands/Uindexed/src/fuor.ads
@@ -1,18 +1,18 @@
 pragma Ada_2012;
 
 package FUOR is
-   
-   type Op_Name is (A, B);   
+
+   type Op_Name is (A, B);
    type Varray is array (Op_Name) of aliased Float;
-   
+
    type Operands is tagged record
       Values : Varray;
    end record
      with Constant_Indexing => C_Indexing;
-   
+
+   function Orelse (Ops : Operands) return Boolean;
    function C_Indexing
      (X : aliased Operands'Class; Op : Op_Name)
      return Float is (X.Values (Op));
-     
-   function Orelse (Ops : Operands) return Boolean;
+
 end;
