a3a70d7388cf22588906b124789cd526d6a951f2
Pierre-Marie de Rodat <derodat@adacore.com>
2020-02-13 11:44:01 +0100

    SCOV.htc: fix flake8 diagnostics
    Change-Id: Ia0cd8f0b9e90133e534c23694c5f51c5032621e4
    TN: S326-007

--- testsuite/SCOV/htc.py
+++ testsuite/SCOV/htc.py
@@ -1,16 +1,13 @@
-# ****************************************************************************
-# ** HARNESS TESTCASE abstraction **
-# ****************************************************************************
+"""HARNESS TESTCASE abstraction
 
-# This module exposes the HarnessTestCase class, designed to automate all the
-# processing required to run a source coverage qualification testcase setup
-# where test.py was found, but without requiring that subprocesses (xcov, xrun,
-# ...) finish without error.
+This module exposes the HarnessTestCase class, designed to automate all the
+processing required to run a source coverage qualification testcase setup where
+test.py was found, but without requiring that subprocesses (xcov, xrun, ...)
+finish without error.
 
-# The aim is to verify that the testsuite detects mismatch between tests
-# expectations and actual coverage results.
-
-# ****************************************************************************
+The aim is to verify that the testsuite detects mismatch between tests
+expectations and actual coverage results.
+"""
 
 import re
 
@@ -19,12 +16,6 @@ from SCOV.tctl import CAT
 from SUITE.cutils import contents_of
 from SUITE.tutils import thistest
 
-
-# =======================
-# == HarnessDiagnostic ==
-# =======================
-
-# A testcase "error" text reported or expected in the Harness output.
 
 class HarnessDiagnostic:
     """
@@ -42,20 +33,12 @@ class HarnessDiagnostic:
         return self.text in reported.text
 
 
-# =====================
-# == HarnessTestCase ==
-# =====================
-
 class HarnessTestCase(TestCase):
-
-    def __init__(
-        self,
-        expected_diags,
-        extradrivers="", extracargs="", category=CAT.auto
-    ):
+    def __init__(self, expected_diags, extradrivers="", extracargs="",
+                 category=CAT.auto):
         TestCase.__init__(self, extradrivers, extracargs, category)
 
-        # By default, these test cases expect failures from subprocesses.
+        # By default, these test cases expect failures from subprocesses
         self.expect_failures = True
 
         self.expected_diags = expected_diags
@@ -71,28 +54,24 @@ class HarnessTestCase(TestCase):
         thistest.flush()
 
         self.reported_diags = [
-            HarnessDiagnostic (text = errtext)
-            for errtext in re.findall (
-                pattern = "^  \* (?:exception|\(.*\)) .*",
-                string  = contents_of (thistest.report.report_file),
-                flags   = re.M
-                )
-            ]
+            HarnessDiagnostic(text=errtext)
+            for errtext in re.findall(
+                pattern=r"^  \* (?:exception|\(.*\)) .*",
+                string=contents_of(thistest.report.report_file),
+                flags=re.M)]
 
-        [self.__count_match_on (reported, expected)
-         for reported in self.reported_diags for expected in self.expected_diags
-         if expected.match(reported)]
+        for reported in self.reported_diags:
+            for expected in self.expected_diags:
+                if expected.match(reported):
+                    self.__count_match_on(reported, expected)
 
-        [thistest.fail_if (
-                expected.nmatches != 1,
-                "expectation check failed - %s" % expected.text
-                )
-         for expected in self.expected_diags]
+        for expected in self.expected_diags:
+            thistest.fail_if(expected.nmatches != 1,
+                             "expectation check failed - %s" % expected.text)
 
         for i in self.reported_diags:
             print i.__dict__
 
-        [thistest.fail_if (
-                reported.nmatches != 1,
-                "reporting check failed - %s" % reported.text)
-         for reported in self.reported_diags]
+        for reported in self.reported_diags:
+            thistest.fail_if(reported.nmatches != 1,
+                             "reporting check failed - %s" % reported.text)
