2c4c03b6f9a76540cf9f6f2b9701af5269e09235
Pierre-Marie de Rodat <derodat@adacore.com>
2020-12-01 12:47:41 +0100

    Instrument: fix handling of pragmas after compilation units
    Change-Id: I4498dcd59968d7556d0e6d12af74a2cdea60dd54
    TN: TB30-010

--- tools/gnatcov/instrument-tree.adb
+++ tools/gnatcov/instrument-tree.adb
@@ -3377,7 +3377,9 @@ package body Instrument.Tree is
          declare
             N : constant Ada_Node := L.Child (J);
          begin
-            Current_Insertion_Info.Index := J;
+            if Current_Insertion_Info.Method in Statement | Declaration then
+               Current_Insertion_Info.Index := J;
+            end if;
             Traverse_One (N);
          end;
       end loop;
