469de085259d1063bcd8d2dd685015e248f0ba5b
creuse <creuse@adacore.com>
2020-12-16 15:33:29 +0100

    altrun/trace32: update t32api strings to python3
    Change-Id: I3cccf09679618d58e09ceae3552168b7505d2416
    TN: TA27-037

--- testsuite/altrun/trace32/t32api.py
+++ testsuite/altrun/trace32/t32api.py
@@ -45,7 +45,7 @@ else:
 
 def cmd_wrapper(cmd, wait_for_completion=False):
 
-    t32.T32_Cmd(cmd)
+    t32.T32_Cmd(cmd.encode())
 
     if wait_for_completion:
         state = ctypes.c_int()
@@ -63,9 +63,9 @@ def cmd_wrapper(cmd, wait_for_completion=False):
 # -------------
 
 def connect(node="localhost", port=20000, packlen=1024):
-    t32.T32_Config(b"NODE=", node)
-    t32.T32_Config(b"PORT=", str(port))
-    t32.T32_Config(b"PACKLEN=", str(packlen))
+    t32.T32_Config(b"NODE=", node.encode())
+    t32.T32_Config(b"PORT=", str(port).encode())
+    t32.T32_Config(b"PACKLEN=", str(packlen).encode())
 
     print(' Connecting...')
     for i in range(1, 3):
@@ -101,14 +101,14 @@ def connect(node="localhost", port=20000, packlen=1024):
 # -----------------
 
 def basic_setup():
-    cmd_wrapper(b"RESet")
-    cmd_wrapper(b"SYStem.RESet")
-    cmd_wrapper(b"SYStem.CPU STM32F756ZG")
-    cmd_wrapper(b"SYStem.CONFIG.DEBUGPORTTYPE SWD")
-    cmd_wrapper(b"SYStem.CONFIG.CONNECTOR MIPI20T")
-    cmd_wrapper(b"SYStem.MemAccess DAP")
-    cmd_wrapper(b"SYStem.Option DUALPORT ON")
-    cmd_wrapper(b"SYStem.Up")
+    cmd_wrapper("RESet")
+    cmd_wrapper("SYStem.RESet")
+    cmd_wrapper("SYStem.CPU STM32F756ZG")
+    cmd_wrapper("SYStem.CONFIG.DEBUGPORTTYPE SWD")
+    cmd_wrapper("SYStem.CONFIG.CONNECTOR MIPI20T")
+    cmd_wrapper("SYStem.MemAccess DAP")
+    cmd_wrapper("SYStem.Option DUALPORT ON")
+    cmd_wrapper("SYStem.Up")
 
 
 # ------------------------
@@ -118,31 +118,31 @@ def basic_setup():
 def init_trace_stm32f7():
     # initialize Offchip-Trace
     # DBGMCU_CR
-    cmd_wrapper(b"Data.Set E:0xe0042004 %Long Data.Long(E:0xe0042004)|0xe0")
+    cmd_wrapper("Data.Set E:0xe0042004 %Long Data.Long(E:0xe0042004)|0xe0")
 
     # set PinMux for PE2-6 -AF0",
 
     # RCC_AHB1RST
-    cmd_wrapper(b"Data.Set E:0x40023810 %Long Data.Long(E:0x40023810)&~0x10")
+    cmd_wrapper("Data.Set E:0x40023810 %Long Data.Long(E:0x40023810)&~0x10")
 
     # RCC_AHB1ENR
-    cmd_wrapper(b"Data.Set E:0x40023830 %Long Data.Long(E:0x40023830)|0x10")
+    cmd_wrapper("Data.Set E:0x40023830 %Long Data.Long(E:0x40023830)|0x10")
 
     # GPIOE_MODER
-    cmd_wrapper(b"Data.Set E:0x40021000 %Long" +
+    cmd_wrapper("Data.Set E:0x40021000 %Long" +
                 " Data.Long(E:0x40021000)|0x00002aa0")
 
     # GPIOE_OSPEEDR
-    cmd_wrapper(b"Data.Set E:0x40021008 %Long" +
+    cmd_wrapper("Data.Set E:0x40021008 %Long" +
                 " Data.Long(E:0x40021008)|0x00003ff0")
 
     # GPIOE_AFRL
-    cmd_wrapper(b"Data.Set E:0x40021020 %Long" +
+    cmd_wrapper("Data.Set E:0x40021020 %Long" +
                 " Data.Long(E:0x40021020)&0xf00000ff")
 
-    cmd_wrapper(b"TPIU.PortSize 4", wait_for_completion=True)
-    cmd_wrapper(b"ETM.Trace ON", wait_for_completion=True)
-    cmd_wrapper(b"ETM.ON", wait_for_completion=True)
+    cmd_wrapper("TPIU.PortSize 4", wait_for_completion=True)
+    cmd_wrapper("ETM.Trace ON", wait_for_completion=True)
+    cmd_wrapper("ETM.ON", wait_for_completion=True)
 
 
 # ---------------------
@@ -155,7 +155,7 @@ def load_executable(path):
     # them all.
     clear_breakpoints()
 
-    cmd_wrapper(b"Data.Load.auto \"%s\"" % path, wait_for_completion=True)
+    cmd_wrapper(f"Data.Load.auto \"{path}\"", wait_for_completion=True)
 
 
 # -------------------
@@ -187,15 +187,15 @@ def run_until(symbol, timeout_sec):
     # go.direct. I don't know why at this point.
     cmd_wrapper("Step.Single 1", wait_for_completion=True)
 
-    cmd_wrapper(b"go.direct %s" % symbol)
+    cmd_wrapper(f"go.direct {symbol}")
 
     # Wait for the CPU to stop or timeout
-    cmd_wrapper(b"WAIT !STATE.RUN() %s.s" % str(timeout_sec),
+    cmd_wrapper(f"WAIT !STATE.RUN() {timeout_sec}.s",
                 wait_for_completion=True)
 
     if get_cpu_state() == "running":
         # Stop the CPU
-        cmd_wrapper(b"Break.direct")
+        cmd_wrapper("Break.direct")
         print("!!! TIMEOUT !!!")
 
 
@@ -253,8 +253,8 @@ def read_register_by_name(name):
 # ------------------
 
 def export_trace(path):
-    cmd_wrapper(b"Trace.FLOWPROCESS", wait_for_completion=True)
-    cmd_wrapper(b"Trace.export.BranchFlow %s /NOSYMBOL /CALLER" % path,
+    cmd_wrapper("Trace.FLOWPROCESS", wait_for_completion=True)
+    cmd_wrapper(f"Trace.export.BranchFlow {path} /NOSYMBOL /CALLER",
                 wait_for_completion=True)
 
 
@@ -274,7 +274,7 @@ def CPU_stopped_at_symbol(symbol):
 # -----------------------
 
 def clear_breakpoints():
-    cmd_wrapper(b"Break.Reset")
+    cmd_wrapper("Break.Reset")
 
 
 # ------------------
