dcee53b26336b2e0f3efc9867fc7837b1b36c60f
Matthieu Eyraud <eyraud@adacore.com>
2021-01-28 16:39:09 +0100

    Add a default for dump-trigger
    This fixes a gnatcov crash when --dump-trigger is left unspecified. Manual
    is the default.
    
    Change-Id: I50da6b907098ca2852bed1b95035f30adc88eb84
    TN: U128-029

--- tools/gnatcov/instrument.ads
+++ tools/gnatcov/instrument.ads
@@ -2,7 +2,7 @@
 --                                                                          --
 --                               GNATcoverage                               --
 --                                                                          --
---                     Copyright (C) 2008-2020, AdaCore                     --
+--                     Copyright (C) 2008-2021, AdaCore                     --
 --                                                                          --
 -- GNATcoverage is free software; you can redistribute it and/or modify it  --
 -- under terms of the GNU General Public License as published by the  Free  --
@@ -53,7 +53,7 @@ package Instrument is
 
    type Any_Dump_Config (Channel : Any_Dump_Channel := Any_Dump_Channel'First)
    is record
-      Trigger : Any_Dump_Trigger;
+      Trigger : Any_Dump_Trigger := Manual;
 
       case Channel is
          when Binary_File =>
