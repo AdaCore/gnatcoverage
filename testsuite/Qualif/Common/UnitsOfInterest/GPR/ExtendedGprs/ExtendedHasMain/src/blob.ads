30ad9444d5378a6aa72d17f5740142b11e1db450
Olivier Hainque <hainque@adacore.com>
2021-10-07 14:23:58 +0000

    Remove gratuitous use of Ada.Text_IO in gnatcov tests
    So these tests remain applicable to toolchain configurations
    not featuring this runtime unit, such as GNAT 7.1.2 with a ZFP RTS.
    
    Part of UA06-023 (gnatcov test failures with GNAT 7.1.2)
    
    Change-Id: Ia1a126db2d547963116830bdec88ec5262168ed6
    (cherry picked from commit 264bb7ce0b1b1ac219b119f113d3f595a582a63d)

---
+++ testsuite/Qualif/Common/UnitsOfInterest/GPR/ExtendedGprs/ExtendedHasMain/src/blob.ads
@@ -0,0 +1,8 @@
+--  Simple package that mains of interest can "with"
+--  to do things without resorting to GNAT runtime units.
+
+with System;
+
+package Blob is
+   X, Y : Integer;
+end;
--- testsuite/Qualif/Common/UnitsOfInterest/GPR/Robustness/BadUnitInSwitch/helper-say_hello.adb
+++ testsuite/Qualif/Common/UnitsOfInterest/GPR/Robustness/BadUnitInSwitch/helper-say_hello.adb
@@ -1,7 +1,6 @@
-with Ada.Text_IO; use Ada.Text_IO;
 
 separate (Helper)
 procedure Say_Hello is
 begin
-   Put_Line ("Hello, world!");
+   Hello := True;
 end Say_Hello;
