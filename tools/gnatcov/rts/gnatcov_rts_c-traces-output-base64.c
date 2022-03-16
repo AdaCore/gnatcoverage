e97ef6a0aa301b0335083a36fd54949a86b09de4
Matthieu Eyraud <eyraud@adacore.com>
2022-03-16 09:59:18 +0100

    Flush the output before writing a base64 trace
    This behavior was inadvertently removed by
    21cead26ff19d1b1f5cebb792dcf916a58afcec9. Restore it.
    
    Change-Id: I481064a10b0d484254849647892c576e99296f6a
    TN: U208-020

--- tools/gnatcov/rts/gnatcov_rts_c-traces-output-base64.c
+++ tools/gnatcov/rts/gnatcov_rts_c-traces-output-base64.c
@@ -133,6 +133,7 @@ gnatcov_rts_write_trace_file_base64 (
   gnatcov_rts_string begin_string
     = STR ("== GNATcoverage source trace file ==");
   gnatcov_rts_string end_string = STR ("== End ==");
+  gnatcov_rts_putchar ('\n');
   gnatcov_rts_puts (begin_string);
   gnatcov_rts_generic_write_trace_file (&buffer, buffers, STR (""), 0,
                                         STR (""), write_bytes);
