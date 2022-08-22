cc451868cd5058e9019f562d1703240efb89345f
Olivier Hainque <hainque@adacore.com>
2022-08-22 12:27:03 +0000

    Add "not null" to predicate type def for QuantExpr tests
    Access to predicate functions in Quantified Expression tests
    are expected never to be null. Mention this explicitly in the
    type definition, which will prevent access checks and avoid
    possible CFG analysis warnings on the corresponding cond branch.
    
    This cures a couple of spurious test failures in binary trace
    mode for optimized runs on some targets, is perfectly legit in
    the tests context and doesn't invalidate the original tests
    purpose at all.
    
    We have a separate ticket to investigate the failure mode
    further.
    
    Part of V822-011.
    
    Change-Id: I72af20ce91af83cc60075d04ba50404c73c1fc04

--- testsuite/Qualif/Ada/decision/1_Core/Operands/QuantExpr/src/expr.ads
+++ testsuite/Qualif/Ada/decision/1_Core/Operands/QuantExpr/src/expr.ads
@@ -2,7 +2,7 @@ pragma Ada_2012;
 package Expr is
    type Sequence is array (Integer range <>) of Integer;
 
-   type Predicate is access function (X : Integer) return Boolean;
+   type Predicate is not null access function (X : Integer) return Boolean;
 
    function GT_3 (X : Integer) return Boolean is (X > 3);
 end;
