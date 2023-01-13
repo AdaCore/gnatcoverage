fe2a676d7d2b2e58d3a8226105a1df15d6783344
Olivier Hainque <hainque@adacore.com>
2023-01-13 07:58:03 -0800

    Qualif test for unit selection attributes in non root project
    Our qualification tests for the use of project files
    to select units of interest had essentially
    
    - a set of tests showing proper selection of the projects
      of interest, from command line switches,
    
    - a set of tests showing proper selection of units of interest
      in a project, from attributes in a project file.
    
    The use of attributes in project files was in effect only
    tested within a root project.
    
    This change adds a test to check that the attributes also
    work within a non root project.
    
    Part of T317-055 (qualification material for use of project files)

---
+++ testsuite/Qualif/Common/UnitsOfInterest/GPR/InnerAttr/src/ops/ops-andthen.ads
@@ -0,0 +1,3 @@
+package Ops.Andthen is
+   function Eval (A, B : Boolean) return Boolean;
+end;
---
+++ testsuite/Qualif/Common/UnitsOfInterest/GPR/InnerAttr/test.py
@@ -0,0 +1,58 @@
+from SCOV.tc import TestCase
+from SCOV.tctl import CovControl
+from SUITE.context import thistest
+from SUITE.cutils import Wdir, list_to_file
+from SUITE.tutils import gprfor
+from SUITE.gprutils import gprcov_for
+
+wd = Wdir()
+
+
+def check(subdir, opspkg, xreports):
+
+    wd.to_subdir(subdir)
+
+    ops_gpr = gprfor(
+        mains=[], prjid="ops", srcdirs="../src/ops", extra=opspkg
+    )
+
+    TestCase(category=None).run(covcontrol=CovControl(
+        deps=["../ops.gpr"], units_in=[], xreports = xreports
+    ))
+
+# ops.gpr: no attribute
+
+check(
+    opspkg="",
+    xreports=["ops.adb", "ops.ads", "ops-andthen.adb", "ops-orelse.adb"],
+    subdir="wd_1",
+)
+
+# ops.gpr: for Units use ...
+
+check(
+    opspkg=gprcov_for(units_in=["ops.andthen"]),
+    xreports=["ops-andthen.adb"],
+    subdir="wd_2",
+)
+
+# ops.gpr: for Excluded_Units use ...
+
+check(
+    opspkg=gprcov_for(units_out=["ops.andthen"]),
+    xreports=["ops.ads", "ops.adb", "ops-orelse.adb"],
+    subdir="wd_3",
+)
+
+# ops.gpr: for Included_Units use ... for Excluded_Units use ...
+
+check(
+    opspkg=gprcov_for(
+        units_in=["ops", "ops.orelse", "ops.andthen"],
+        units_out=["ops.orelse"]),
+    xreports=["ops.ads", "ops.adb", "ops-andthen.adb"],
+    subdir="wd_3",
+)
+
+
+thistest.result()
