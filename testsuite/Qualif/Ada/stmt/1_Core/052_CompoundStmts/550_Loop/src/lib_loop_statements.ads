835fcb89909b838bfc77e7227ca50eda2066a40e
Olivier Hainque <hainque@adacore.com>
2023-03-10 02:59:19 -0800

    Robustify Qualif/...550_Loop/src/lib_loop_statements wrt optimization
    Reorder statements to prevent the occurrence of a
    no-code line at the end of a loop sequence. The no-code
    was legit but is annoying to test against. Reordering
    the statements helps that, doesn't invalidate the test
    and doesn't hide any defect.
    
    Take the opportunity to add a few comments in a common
    spec, describing the testcase(s) organisation.
    
    Part of W215-023 (gnatcov 22.x qualification)

--- testsuite/Qualif/Ada/stmt/1_Core/052_CompoundStmts/550_Loop/src/lib_loop_statements.ads
+++ testsuite/Qualif/Ada/stmt/1_Core/052_CompoundStmts/550_Loop/src/lib_loop_statements.ads
@@ -2,6 +2,14 @@
 -- its body elaboration code.
 
 package Lib_Loop_Statements is
+
+   -- An array of integers, all zero for starters. A number of these
+   -- are set to 1 in the package elab body. The number of values so
+   -- initialized is controlled by a variable in a distinct package spec,
+   -- provided by each testcase.
    Values : array (1 .. 8) of Integer := (others => 0);
+
    function N_Ones return Integer;
+   --  Recompute the number of slots set by looking at the values
+
 end;
