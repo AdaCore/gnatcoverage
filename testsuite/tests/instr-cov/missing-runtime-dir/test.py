478db4315101fcd3f522d325eb00b197912d5108
Pierre-Marie de Rodat <derodat@adacore.com>
2023-03-13 10:37:34 +0000

    Fix a crash in the recently introduced unit provider
    Entries in the ADA_INCLUDE_PATH environment variable make the GPR system
    list them as "runtime directories". They are included even if there is
    no such directory, so code that processes them must have defensive code
    for this case.

---
+++ testsuite/tests/instr-cov/missing-runtime-dir/test.py
@@ -0,0 +1,27 @@
+"""
+Check that instrumentation an Ada project does not fail when the
+ADA_INCLUDE_PATH environment variable (classified as a "runtime directory" by
+the GPR system) does not exist.
+"""
+
+import os
+
+from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
+from SUITE.cutils import contents_of, Wdir
+from SUITE.gprutils import GPRswitches
+from SUITE.tutils import thistest, gprfor
+
+
+Wdir("tmp_")
+
+os.environ["ADA_INCLUDE_PATH"] = os.path.abspath("nosuchdir")
+
+build_run_and_coverage(
+    gprsw=GPRswitches(root_project=gprfor(mains=["main.adb"], srcdirs=[".."])),
+    covlevel="stmt",
+    mains=["main"],
+    extra_coverage_args=["--annotate=xcov"],
+)
+check_xcov_reports("*.xcov", {"main.adb.xcov": {"+": {5}}}, "obj")
+
+thistest.result()
