74271dbca04b94f0e8df5790217b56cf3268eb32
Matthieu Eyraud <eyraud@adacore.com>
2023-03-17 11:49:39 +0100

    W315-021-generic-preelab: new regression test
    (cherry picked from commit 9b84a17fdd9ccb6ad45564b0ecef8553456828b5)

---
+++ testsuite/tests/W315-021-generic-preelab/pkg.adb
@@ -0,0 +1,5 @@
+package body Pkg is
+   Var : constant Integer := 2;
+
+   procedure Foo is null;
+end Pkg;
---
+++ testsuite/tests/W315-021-generic-preelab/pkg.ads
@@ -0,0 +1,6 @@
+generic
+   A : Integer;
+package Pkg is
+   pragma Preelaborate;
+   procedure Foo;
+end Pkg;
