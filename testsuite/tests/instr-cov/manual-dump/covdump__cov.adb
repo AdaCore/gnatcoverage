e15ee5fe9e4e3c72d2d2d37e32687517dca007c5
Matthieu Eyraud <eyraud@adacore.com>
2023-06-14 07:28:44 +0200

    Update tests following instrumentation changes

--- testsuite/tests/instr-cov/manual-dump/covdump__cov.adb
+++ testsuite/tests/instr-cov/manual-dump/covdump__cov.adb
@@ -1,11 +1,11 @@
 with Ada.Command_Line;
 
-with GNATcov_RTS.Buffers.Lists.P;
+with GCVRT.P;
 with GNATcov_RTS.Traces.Output.Files;
 
 procedure Covdump is
 begin
    GNATcov_RTS.Traces.Output.Files.Write_Trace_File
-     (Buffers_Groups => GNATcov_RTS.Buffers.Lists.P.List,
+     (Buffers_Groups => GCVRT.P.List,
       Filename       => Ada.Command_Line.Command_Name & ".srctrace");
 end Covdump;
