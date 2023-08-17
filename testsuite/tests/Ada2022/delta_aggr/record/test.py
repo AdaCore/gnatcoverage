9789e7b8a97e24518b1781fba7576fe5d5a2ea2c
Léo Creuse <creuse@adacore.com>
2023-08-17 12:25:14 +0200

    Add test verifying delta aggregate support

---
+++ testsuite/tests/Ada2022/delta_aggr/record/test.py
@@ -0,0 +1,11 @@
+"""
+Test the correct handling and instrumentation of record delta aggregates
+"""
+
+from SCOV.tc import TestCase
+from SCOV.tctl import CAT
+from SUITE.context import thistest
+
+
+TestCase(category=CAT.decision).run()
+thistest.result()
