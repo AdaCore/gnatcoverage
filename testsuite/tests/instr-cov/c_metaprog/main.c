6f835aece76dde10c146f98446cd9ce139459493
Matthieu Eyraud <eyraud@adacore.com>
2023-09-18 18:03:31 +0200

    Instrument.C: fix issue related to metaprogramming instances
    Do not instrument decisions in sources of interest whose parent
    statement does not belong to a source of interest. This can happen with
    metaprogramming instances in C/C++.

---
+++ testsuite/tests/instr-cov/c_metaprog/main.c
@@ -0,0 +1,8 @@
+int
+main ()
+{
+  int a = 0;
+#define DECISION a
+#include "meta_print_msg.h"
+  return 0;
+}
---
+++ testsuite/tests/instr-cov/c_metaprog/meta_print_msg.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+#ifdef DECISION
+if (DECISION)
+  printf("Hello world!\n");
+#endif
