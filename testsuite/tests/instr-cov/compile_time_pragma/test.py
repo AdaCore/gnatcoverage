ee233210744b185d148e9ad36484a3bc9d712cbc
Pierre-Marie de Rodat <derodat@adacore.com>
2023-10-02 13:25:07 +0000

    Instrument Ada: fix handling of Compile_Time_* pragmas
    The boolean expressions for the Compile_Time_Error and
    Compile_Time_Warning pragmas are expected to be time known. This means
    that we cannot instrument them as decision/conditions, as doing so would
    make them non compile time known. Emitting coverage obligations for
    compile time known expressions would not make much sense anyway.

---
+++ testsuite/tests/instr-cov/compile_time_pragma/test.py
@@ -0,0 +1,25 @@
+"""
+Check that the decision/conditions of Compile_Time_Error pragmas are not
+instrumented.
+"""
+
+import os
+import os.path
+
+from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
+from SUITE.context import thistest
+from SUITE.cutils import Wdir
+from SUITE.tutils import gprfor
+from SUITE.gprutils import GPRswitches
+
+tmp = Wdir("tmp_")
+
+build_run_and_coverage(
+    gprsw=GPRswitches(root_project=gprfor(mains=["main.adb"], srcdirs=[".."])),
+    covlevel="stmt+mcdc",
+    mains=["main"],
+    extra_coverage_args=["-axcov", "--output-dir=xcov"],
+)
+check_xcov_reports("*.xcov", {"main.adb.xcov": {"+": {8}}}, "xcov")
+
+thistest.result()
