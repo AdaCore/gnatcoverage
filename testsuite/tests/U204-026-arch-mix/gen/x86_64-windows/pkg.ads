a25ef9ae7b8b46a7b9f744da8978b90069ffe345
Léo Creuse <creuse@adacore.com>
2024-01-15 14:52:21 +0100

    Bump trace format version
    The trace entries used to contain the name of the project to which
    the trace entry's unit belonged. This was removed by
    b567aca989fa1a718f80987d9c45e5dfb4bb1839, but the change did not
    bump the trace format version despite being a breaking change.
    
    This change bumps the version number after the fact to avoid potential
    confusing error messages in the future.

--- testsuite/tests/U204-026-arch-mix/gen/x86_64-windows/pkg.ads
+++ testsuite/tests/U204-026-arch-mix/gen/x86_64-windows/pkg.ads
@@ -1,5 +1,3 @@
-pragma Style_Checks (Off); pragma Warnings (Off);
-with GNATcov_RTS.Buffers;with GCVRT.Pz3791e32c;package Pkg is
+pragma Style_Checks (Off); pragma Warnings (Off);with GNATcov_RTS.Buffers;with GCVRT.Pz3791e32c;package Pkg is
    procedure Compute (A, B : Boolean);
 end Pkg;
-
