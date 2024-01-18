321ddf430d80ae0f7171399308fe1f74a69d8970
Pierre-Marie de Rodat <derodat@adacore.com>
2024-01-18 13:29:25 +0000

    instr-cov/208-no-secsta: new testcase
    (cherry picked from commit 85f4dbb46766d8195c253a58e2e31cbfcd9bc3a5)

---
+++ testsuite/tests/instr-cov/208-no-secsta/test.py
@@ -0,0 +1,39 @@
+"""
+Check that projects with the No_Secondary_Stack restriction compile fine after
+instrumentation.
+"""
+
+import os.path
+
+from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
+from SUITE.context import thistest
+from SUITE.cutils import Wdir
+from SUITE.gprutils import GPRswitches
+from SUITE.tutils import gprfor
+
+tmp = Wdir("tmp_")
+
+p = gprfor(
+    srcdirs=[".."],
+    mains=["main.adb"],
+    compiler_extra='for Local_Configuration_Pragmas use "../gnat.adc";',
+)
+build_run_and_coverage(
+    gprsw=GPRswitches(root_project=p),
+    covlevel="stmt+mcdc",
+    mains=["main"],
+    extra_coverage_args=["-axcov", "--output-dir=xcov"],
+)
+check_xcov_reports(
+    "*.xcov",
+    {
+        "main.adb.xcov": {
+            "+": {12},
+            "!": {9, 14, 16},
+            "-": {15, 17},
+        },
+    },
+    cwd="xcov",
+)
+
+thistest.result()
