7c910446bac7bca955e87ea60e114bc208f739a1
Léo Creuse <creuse@adacore.com>
2024-01-19 13:33:38 +0100

    Add trace counter in the trace filenames
    This allows multiple traces to be dumped in short succession without
    having one trace being overwritten by the next one.

--- testsuite/tests/instr-cov/manual-dump/ada_only/test.py
+++ testsuite/tests/instr-cov/manual-dump/ada_only/test.py
@@ -4,14 +4,13 @@ indicating where to dump the coverage buffers, gnatcov is able to replace it
 with a call to the dump buffers procedure and output correct traces.
 """
 
-import os
-import os.path
+import glob
 
-from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
+from SCOV.minicheck import build_and_run, check_xcov_reports, xcov
 from SUITE.context import thistest
 from SUITE.cutils import Wdir, contents_of
 from SUITE.gprutils import GPRswitches
-from SUITE.tutils import gprfor
+from SUITE.tutils import gprfor, srctrace_pattern_for
 
 tmp = Wdir('tmp_')
 
@@ -29,14 +28,15 @@ lib2_p = gprfor(mains=[],
                 langs=["Ada"],
                 )
 
-p = gprfor(mains=["main.adb"],
+p = gprfor(prjid="gen",
+           mains=["main.adb"],
            srcdirs=["../src"],
            objdir="obj",
            deps=["lib1", "lib2"])
 
 instr_warning = (r"warning: Manual dump trigger indications were found in.*")
 
-build_run_and_coverage(
+cov_args = build_and_run(
     gprsw=GPRswitches(root_project=p, units=["lib1", "main"]),
     covlevel='stmt',
     mains=['main'],
@@ -59,8 +59,13 @@ def check_call(file):
 
 check_call('obj-lib2/lib2-gnatcov-instr/lib2.adb')
 
-# Check that we get the expected coverage reports
+# Generate and check coverage reports, using the last trace generated for the
+# root project.
+trace_file = sorted(glob.glob(srctrace_pattern_for(
+    "main", manual=True, manual_prj_name="gen"
+)))[-1]
 
+xcov(cov_args + [trace_file], out="coverage.log")
 check_xcov_reports('xcov/*.xcov', {
     'xcov/main.adb.xcov': {'+': {12, 15, 19, 24, 26, 27, 28},
                            '-': {21, 29}},
