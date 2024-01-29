3c1468eeea1d57b41649b753ed0308791af38aaa
Pierre-Marie de Rodat <derodat@adacore.com>
2024-01-29 16:41:42 +0000

    Workaround a GNATCOLL.Projects.Other_File bug for Ada runtime subunits
    (cherry picked from commit b12f8feb9b656d9805a220061f5377d34c6d31d8)

---
+++ testsuite/tests/instr-cov/217-runtime-subunit/runtime/gnarl/s-taprop.ads
@@ -0,0 +1,5 @@
+package System.Task_Primitives.Operations is
+   pragma Preelaborate;
+   procedure Dummy;
+   function Register_Foreign_Thread return Integer;
+end System.Task_Primitives.Operations;
---
+++ testsuite/tests/instr-cov/217-runtime-subunit/runtime/gnat/ada.ads
@@ -0,0 +1,4 @@
+package Ada is
+   pragma No_Elaboration_Code_All;
+   pragma Pure;
+end Ada;
