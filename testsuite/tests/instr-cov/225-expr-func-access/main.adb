4a5e727eaf54f2e9d63468cb550b108dd31f4ac7
Pierre-Marie de Rodat <derodat@adacore.com>
2024-02-12 11:50:19 +0000

    instr-cov/225-expr-func-access: rework to avoid bin/src traces diffs
    Put the expression functions in a body to prevent inlining from
    affecting coverage reports with binary traces. This fixes the current
    discrepancies with source traces.

--- testsuite/tests/instr-cov/225-expr-func-access/main.adb
+++ testsuite/tests/instr-cov/225-expr-func-access/main.adb
@@ -2,22 +2,9 @@ pragma Ada_2022;
 
 with Ada.Text_IO; use Ada.Text_IO;
 
+with Pkg; use Pkg;
+
 procedure Main is
-
-   type Integer_Access is access all Integer;
-
-   A : aliased Integer;
-   B : aliased Integer;
-
-   function Get_Ref (Is_A : Boolean) return Integer_Access
-   is (if Is_A then A'Access else B'Access);
-
-   function Get_Unc_Ref (Is_A : Boolean) return Integer_Access
-   is (if Is_A then A'Unchecked_Access else B'Unchecked_Access);
-
-   function Get_Unr_Ref (Is_A : Boolean) return Integer_Access
-   is (if Is_A then A'Unrestricted_Access else B'Unrestricted_Access);
-
 begin
    Get_Ref (True).all := 1;
    Get_Ref (False).all := 2;
