499ae35252d61e5c61810e2e1e034c2a21e52719
Pierre-Marie de Rodat <derodat@adacore.com>
2024-02-19 18:59:46 +0000

    integrated_instr/duplicated_symbols_in_libraries: minor refactoring

--- testsuite/tests/integrated_instrumentation/duplicated_symbols_in_libraries/test.py
+++ testsuite/tests/integrated_instrumentation/duplicated_symbols_in_libraries/test.py
@@ -81,14 +81,11 @@ thistest.fail_if_no_match(
     f'\r?\n  {os.path.join(cwd, "libbar", "bar.c")}',
     contents_of(log_file),
 )
-check_xcov_reports(
-    "*.xcov",
-    {
-        "libbar-bar.c.xcov": {"+": {4}},
-        "libbar-foo.c.xcov": {"+": {4}},
-        "libfoo-bar.c.xcov": {"-": {4}},
-        "libfoo-foo.c.xcov": {"-": {4}},
-    },
-)
+check_xcov_reports(".", {
+    "libbar-bar.c.xcov": {"+": {4}},
+    "libbar-foo.c.xcov": {"+": {4}},
+    "libfoo-bar.c.xcov": {"-": {4}},
+    "libfoo-foo.c.xcov": {"-": {4}},
+})
 
 thistest.result()
