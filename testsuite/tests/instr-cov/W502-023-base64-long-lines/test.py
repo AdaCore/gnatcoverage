f8bfd7d35c6f6feaaa0c1d5ee8fff01bbf52bbc8
Pierre-Marie de Rodat <derodat@adacore.com>
2024-03-01 17:14:44 +0000

    instr-cov/W502-023-base64-long-lines: stabilize for arm-elf
    Something in the pipeline (the Ada runtime? Qemu?) has trouble handling
    the very long lines that the test program writes on the serial port,
    making this test unstable on arm-elf.
    
    Since the point of this testcase is to make sure that "gnatcov
    extract-base64-trace" does not crash (regardless of how the input text
    file was created), rework it so that the very long line is written by
    Python, which should fix the unstability.
    
    (cherry picked from commit f2be5ab3fd674343a06b29d71265358ea12a158c)

--- testsuite/tests/instr-cov/W502-023-base64-long-lines/test.py
+++ testsuite/tests/instr-cov/W502-023-base64-long-lines/test.py
@@ -3,15 +3,16 @@ Check that "gnatcov extract-base64-trace" handles correctly input files with
 very long lines (it used to crash with a stack overflow).
 """
 
-from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
-from SUITE.cutils import Wdir
+from SCOV.minicheck import build_and_run, check_xcov_reports
+from SUITE.cutils import Wdir, contents_of
 from SUITE.gprutils import GPRswitches
-from SUITE.tutils import thistest, gprfor
+from SUITE.tutils import gprfor, thistest, xcov
 
 
 Wdir("tmp_")
 
-build_run_and_coverage(
+# First produce a source trace from the test program
+xcov_args = build_and_run(
     gprsw=GPRswitches(root_project=gprfor(mains=["main.adb"], srcdirs=[".."])),
     covlevel="stmt",
     mains=["main"],
@@ -21,6 +22,31 @@ build_run_and_coverage(
     dump_channel="base64-stdout",
 )
 
-check_xcov_reports("*.xcov", {"main.adb.xcov": {"+": {5, 6}}}, "obj")
+# Then produce an alternate base64 trace with very long lines
+with open("base64_long_lines.txt", "w") as f:
+    for _ in range(10000):
+        f.write("A" * 1000)
+    f.write("\n")
+    f.write(contents_of("main_output.txt"))
+
+# Extract the source trace from base64
+xcov(
+    ["extract-base64-trace", "base64_long_lines.txt", xcov_args[-1]],
+    out="extract.txt",
+)
+thistest.fail_if_not_equal(
+    "'gnatcov extract-base64-trace' output",
+    "",
+    contents_of("extract.txt"),
+)
+
+# Produce the coverage report and check its contents
+xcov(xcov_args, out="coverage.txt")
+thistest.fail_if_not_equal(
+    "'gnatcov coverage' output",
+    "",
+    contents_of("coverage.txt"),
+)
+check_xcov_reports("*.xcov", {"main.adb.xcov": {"+": {5}}}, "obj")
 
 thistest.result()
