a1886bdd82f19203e906b5e2df6ad636d16b1506
Dorian Péron <peron@adacore.com>
2024-05-16 10:37:11 +0000

    llvm-json: Parse the JSON file into a Report object

--- tools/gnatcov/llvm_json_checkpoints.ads
+++ tools/gnatcov/llvm_json_checkpoints.ads
@@ -16,9 +16,78 @@
 -- of the license.                                                          --
 ------------------------------------------------------------------------------
 
+with Ada.Containers.Vectors;
+
+with Coverage.Source; use Coverage.Source;
+with Logging;
+with SC_Obligations;  use SC_Obligations;
+with Slocs;           use Slocs;
+with Strings;         use Strings;
+
 package LLVM_JSON_Checkpoints is
 
-   procedure JSON_Load (Filename : String);
+   LLVM_Trace : constant Logging.GNATCOLL_Trace :=
+      Logging.Create_Trace ("LLVM-JSON");
+   --  Trace to log LLVM JSON Trace Adapter messages.
+
+   type LLVM_Region_Kind is (Statement, Decision, Condition);
+
+   type LLVM_Region_Id is new Natural;
+   No_LLVM_Region_Id : constant LLVM_Region_Id := 0;
+   subtype Valid_LLVM_Region_Id is LLVM_Region_Id
+      range No_LLVM_Region_Id + 1 .. LLVM_Region_Id'Last;
+
+   type LLVM_Region (Kind : LLVM_Region_Kind := Statement) is record
+      Span : Local_Source_Location_Range;
+      Id   : Valid_LLVM_Region_Id;
+      SCO  : SCO_Id := No_SCO_Id;
+      --  SCO_Id is meant to be set by the SC_obligations pass when
+      --  registering SCOs.
+
+      case Kind is
+         when Statement =>
+            Execution_Count : Natural;
+
+         when Decision =>
+            Num_Conditions : Natural;
+            Test_Vectors   : Evaluation_Vectors.Vector;
+            Next_Condition : Condition_Index := Condition_Index'First;
+            --  Used for assigning a condition index to its conditions
+
+         when Condition =>
+            Parent_Id : Valid_LLVM_Region_Id;
+            Index     : Condition_Index;
+      end case;
+   end record;
+
+   package LLVM_Region_Vector is new Ada.Containers.Vectors
+     (Index_Type   => Valid_LLVM_Region_Id,
+      Element_Type => LLVM_Region);
+
+   type LLVM_Coverage_Function_Ckpt is record
+      Name         : Unbounded_String;
+      Mangled_Name : Unbounded_String;
+      Regions      : LLVM_Region_Vector.Vector;
+   end record;
+
+   package LLVM_Coverage_Function_Ckpt_Vector is new Ada.Containers.Vectors
+     (Index_Type   => Natural,
+      Element_Type => LLVM_Coverage_Function_Ckpt);
+
+   type LLVM_Coverage_File_Ckpt is record
+      Filename  : Unbounded_String;
+      Functions : LLVM_Coverage_Function_Ckpt_Vector.Vector;
+   end record;
+
+   package LLVM_Coverage_File_Ckpt_Vector is new Ada.Containers.Vectors
+     (Index_Type   => Natural,
+      Element_Type => LLVM_Coverage_File_Ckpt);
+
+   type LLVM_Coverage_Ckpt is record
+      File_Reports : LLVM_Coverage_File_Ckpt_Vector.Vector;
+   end record;
+
+   procedure JSON_Load (JSON_Filename : String);
    --  Read a JSON file containing LLVM coverage data
    --  and populate internal structures with it.
 
