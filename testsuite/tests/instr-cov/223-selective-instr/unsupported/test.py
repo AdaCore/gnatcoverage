7230026ab09addb6620b5fd5b37d0372e082046c
Matthieu Eyraud <eyraud@adacore.com>
2024-08-19 11:45:48 +0200

    selective-instr: adapt test to make it work for cross targets

--- testsuite/tests/instr-cov/223-selective-instr/unsupported/test.py
+++ testsuite/tests/instr-cov/223-selective-instr/unsupported/test.py
@@ -5,6 +5,7 @@ instrumentation of a program with annotations that are not the same as the
 current source instrumentation data.
 """
 
+import os
 import os.path
 
 from SCOV.minicheck import build_and_run, check_xcov_reports
@@ -17,41 +18,48 @@ Wdir("tmp_")
 
 # Generate the root project
 root_project = gprfor(srcdirs=[os.path.join("..")], mains=["test.c"])
+gprsw = GPRswitches(root_project)
 
 # Configure the build with -DA which results in the inclusion of an annotated
 # region at pkg.c from line 3 to line 6.
-build_and_run(
-    gprsw=GPRswitches(root_project),
-    covlevel="stmt",
-    mains=["test"],
-    extra_instr_args=[
-        "--c-opts=-DA",
-        "--dump-filename-simple",
-        "--dump-filename-prefix=a",
-    ],
-    extra_coverage_args=["-axcov", "--output-dir=xcov"],
-    trace_mode="src",
-    dump_trigger="main-end",
+tmp_a = Wdir("a")
+trace_a = os.path.abspath(
+    build_and_run(
+        gprsw=gprsw,
+        covlevel="stmt",
+        mains=["test"],
+        extra_instr_args=["--c-opts=-DA"],
+        gpr_exe_dir="..",
+        extra_coverage_args=[],
+        trace_mode="src",
+    )[-1]
 )
+tmp_a.to_homedir()
 
 # Configure the build with -DB which results in the inclusion of an annotated
 # region at pkg.c from line 8 to line 11, thus differing from the above.
-cov_args = build_and_run(
-    gprsw=GPRswitches(root_project),
-    covlevel="stmt",
-    mains=["test"],
-    extra_instr_args=[
-        "--c-opts=-DB",
-        "--dump-filename-simple",
-        "--dump-filename-prefix=b",
-    ],
-    extra_coverage_args=["-axcov", "--output-dir=xcov"],
-    trace_mode="src",
-    dump_trigger="main-end",
+tmp_b = Wdir("b")
+trace_b = os.path.abspath(
+    build_and_run(
+        gprsw=gprsw,
+        covlevel="stmt",
+        mains=["test"],
+        extra_instr_args=["--c-opts=-DB"],
+        trace_mode="src",
+        gpr_exe_dir="..",
+        extra_coverage_args=[],
+    )[-1]
 )
+tmp_b.to_homedir()
 
-# Now, try to generate a coverage report from both traces.
-xcov(cov_args + ["a.srctrace", "b.srctrace"], out="cov.out")
+# Now, try to generate a coverage report from both traces
+os.mkdir("xcov")
+xcov(
+    ["coverage"]
+    + gprsw.cov_switches
+    + ["-axcov", "--output-dir=xcov", "--level=stmt", trace_a, trace_b],
+    out="cov.out",
+)
 
 # Check that gnatcov warns about inconsistency between a.srctrace and the
 # source instrumentation data.
