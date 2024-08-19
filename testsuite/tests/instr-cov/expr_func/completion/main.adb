740aece73d6f120cd3a637e8a909bf503ceb2396
Matthieu Eyraud <eyraud@adacore.com>
2024-08-19 14:38:03 +0200

    Fix detection of non-instrumentable expression function

---
+++ testsuite/tests/instr-cov/expr_func/completion/main.adb
@@ -0,0 +1,8 @@
+pragma Ada_2012;
+with Pak; use Pak;
+
+procedure Main is
+   T_Instance  : T := Make (True);
+begin
+   null;
+end Main;
---
+++ testsuite/tests/instr-cov/expr_func/completion/pak.ads
@@ -0,0 +1,19 @@
+pragma Ada_2012;
+
+package Pak is
+
+   type T is private;
+
+   function Make (Cond : Boolean) return T;
+
+private
+   type T is tagged record
+      X : Integer;
+   end record;
+   function Make (Cond : Boolean) return T is (T'(X => (if Cond then 1 else 2)));
+
+   type TT is new T with record
+      Y : Integer;
+   end record;
+   overriding function Make (Cond : Boolean) return TT is (TT'(X => 3, Y => 4));
+end Pak;
