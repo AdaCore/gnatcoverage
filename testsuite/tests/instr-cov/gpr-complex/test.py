bd069deceff4ac6c3b9bbcc5dcf41ae6b8551538
Pierre-Marie de Rodat <derodat@adacore.com>
2024-09-26 11:55:11 +0000

    instr-cov/gpr-complex: refactor testcase logic
    No change of behavior expected.
    
    (cherry picked from commit 2f4884b7162f097cbbf4869b9c7b8dc68bb78452)

--- testsuite/tests/instr-cov/gpr-complex/test.py
+++ testsuite/tests/instr-cov/gpr-complex/test.py
@@ -13,7 +13,7 @@ from SCOV.instr import xcov_instrument
 from SCOV.minicheck import build_run_and_coverage, check_xcov_reports
 from SUITE.context import thistest
 from SUITE.control import env
-from SUITE.cutils import no_ext, Wdir
+from SUITE.cutils import Wdir
 from SUITE.gprutils import GPRswitches
 from SUITE.tutils import gprbuild, gprinstall
 
@@ -46,36 +46,45 @@ env.add_search_path(
 )
 
 
-def build_run_cov_and_check(main_prj, main_prj_obj_dir, expected_xcov):
+def check(label, name, expected_xcov):
+    """
+    Copy the "name" project from the test directory to the current directory
+    and run it through the instrument/build/run/coverage steps. Check that the
+    coverage report produced matches "expected_cov".
+    """
+    thistest.log(f"== {label} ({name}) ==")
+
+    main_prj = f"{name}.gpr"
+    obj_dir = f"obj-{name}"
+
     cp(os.path.join("..", main_prj), ".", recursive=True)
-    cp(os.path.join("..", f"src-{no_ext(main_prj)}"), ".", recursive=True)
+    cp(os.path.join("..", f"src-{name}"), ".", recursive=True)
+
     build_run_and_coverage(
         gprsw=GPRswitches(root_project=os.path.join(main_prj)),
         covlevel="stmt",
         mains=["main"],
-        gpr_obj_dir=main_prj_obj_dir,
-        gpr_exe_dir=main_prj_obj_dir,
+        gpr_obj_dir=obj_dir,
+        gpr_exe_dir=obj_dir,
         extra_coverage_args=[
             "--annotate=xcov",
             "--externally-built-projects",
             "--units=pkg",
         ],
     )
-    check_xcov_reports(main_prj_obj_dir, expected_xcov)
+    check_xcov_reports(obj_dir, expected_xcov)
 
 
 # Try the simple case: the main project extends the library, but does not
 # redefine anything.
-thistest.log("== Simple case ==")
-expected_xcov = {
-    "pkg.ads.xcov": {},
-    "pkg.adb.xcov": {"+": {5}},
-    "pkg-bar.adb.xcov": {"+": {3}},
-}
-build_run_cov_and_check(
-    "main_simple.gpr",
-    main_prj_obj_dir="obj-main_simple",
-    expected_xcov=expected_xcov,
+check(
+    "Simple case",
+    "main_simple",
+    expected_xcov={
+        "pkg.ads.xcov": {},
+        "pkg.adb.xcov": {"+": {5}},
+        "pkg-bar.adb.xcov": {"+": {3}},
+    },
 )
 
 # Now the main project defines an alternate body. TODO! as we reinstrument the
@@ -83,43 +92,37 @@ build_run_cov_and_check(
 # redefined), we end up instrumenting the instrumented version for sources
 # belonging to the library. We should fix this (and adjust coverage
 # expectations) if possible.
-thistest.log("== Redefining Pkg body ==")
-expected_xcov = {
-    "pkg.ads.xcov": {},
-    "pkg.adb.xcov": {"+": {7}},
-    "pkg-bar.adb.xcov": {"+": {3}},
-}
-build_run_cov_and_check(
-    "main_body.gpr",
-    main_prj_obj_dir="obj-main_body",
-    expected_xcov=expected_xcov,
+check(
+    "Redefining Pkg body",
+    "main_body",
+    expected_xcov={
+        "pkg.ads.xcov": {},
+        "pkg.adb.xcov": {"+": {7}},
+        "pkg-bar.adb.xcov": {"+": {3}},
+    },
 )
 
 
 # Now the main project defines an alternate separate.
-thistest.log("== Redefining Pkg.Bar separate ==")
-expected_xcov = {
-    "pkg.ads.xcov": {},
-    "pkg.adb.xcov": {"+": {5}},
-    "pkg-bar.adb.xcov": {"+": {5}},
-}
-build_run_cov_and_check(
-    "main_sep.gpr",
-    main_prj_obj_dir="obj-main_sep",
-    expected_xcov=expected_xcov,
+check(
+    "Redefining Pkg.Bar separate",
+    "main_sep",
+    expected_xcov={
+        "pkg.ads.xcov": {},
+        "pkg.adb.xcov": {"+": {5}},
+        "pkg-bar.adb.xcov": {"+": {5}},
+    },
 )
 
 # Now the main project defines an alternate spec.
-thistest.log("== Redefining Pkg spec ==")
-expected_xcov = {
-    "pkg.adb.xcov": {"+": {5}},
-    "pkg-bar.adb.xcov": {"+": {3}},
-    "pkg.ads.xcov": {"+": {4}},
-}
-build_run_cov_and_check(
-    "main_spec.gpr",
-    main_prj_obj_dir="obj-main_spec",
-    expected_xcov=expected_xcov,
+check(
+    "Redefining Pkg spec",
+    "main_spec",
+    expected_xcov={
+        "pkg.adb.xcov": {"+": {5}},
+        "pkg-bar.adb.xcov": {"+": {3}},
+        "pkg.ads.xcov": {"+": {4}},
+    },
 )
 
 thistest.result()
