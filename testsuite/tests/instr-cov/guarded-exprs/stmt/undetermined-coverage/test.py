0f898736de49eca1d91de275291648b81138beff
Dorian Péron <peron@adacore.com>
2024-11-29 12:44:08 +0100

    tests(gexpr): Add test for undetermined coverage

---
+++ testsuite/tests/instr-cov/guarded-exprs/stmt/undetermined-coverage/test.py
@@ -0,0 +1,13 @@
+"""
+Regression test: Check the "undetermined coverage" for guarded expressions.
+"""
+
+from SCOV.tc import TestCase
+from SUITE.context import thistest
+
+TestCase(
+    gexpr_lvl=True,
+    tolerate_messages="warning: Guarded Expression coverage is not"
+    " available before Ada2022",
+).run()
+thistest.result()
