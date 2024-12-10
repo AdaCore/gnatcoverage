d6759b19cff0853bfc7c7ef09c44980c8d879d7c
Viviane Garèse <garese@adacore.com>
2024-12-10 14:46:46 +0100

    Add support for user operators for call coverage

--- testsuite/tests/287-function_call_cov/user_operator/src/op.adb
+++ testsuite/tests/287-function_call_cov/user_operator/src/op.adb
@@ -1,13 +1,66 @@
 pragma Ada_2012;
 
-with Ada.Text_IO; use Ada.Text_IO;
+with Reversed; use Reversed;
 
 procedure Op
 is
-    function "+" (L, R : Boolean) return String is ("Executed"); -- # fun
+   T : Boolean := True;                                  -- # decl
+   F : Boolean := False;                                 -- # decl
+   Dummy : Boolean := True;                              -- # decl
 
-    T :          Boolean := True;                                -- # t_decl
-    S : constant String  := T + T;                               -- # op_call
+   function "+" (Left, Right : Boolean) return Boolean   -- # fun
+   is (Left and Right);                                  -- # fun
+
+   function "+" (Arg : Reversed.T) return Reversed.T     -- # fun
+   is (0 - Arg);                                         -- # fun
+
+   function "&" (L,R : Float) return Float               -- # fun
+   is (42.0);                                            -- # fun
+
+   function "&" (L : Integer; R : String) return Integer -- # fun
+   is (42);                                              -- # fun
+
+   function "&" (L : String; R : Integer) return Integer -- # fun
+   is (42);                                              -- # fun
+
 begin
-    Put_Line (S);                                                -- # put_line
+   Dummy := F + T;                                       -- # ok
+   Dummy := "+" (T, F);                                  -- # ok
+   Dummy := F + T + F + T;                               -- # ok
+   Dummy := "+" (T, "+" (T, F));                         -- # ok
+
+   Dummy := One < Two;                                   -- # ok
+   Dummy := Reversed."<" (One, Two);                     -- # ok
+
+   Dummy := +One = -1;                                   -- # ok
+   Dummy := Reversed.Inner."+" (One) = -1;               -- # ok
+
+   Dummy := (40 & "Two") = 42;                           -- # ok
+   Dummy := (1.3 & 3.2 & 2.1) = 42.0;                    -- # ok
+
+   Dummy := F + ("&" ("A", 8) = 42);                     -- # ok
+   Dummy := "+" (F, ("&" ("A", 8) = 42));                -- # ok
+
+   Dummy := ("A" & "B" & 42 & "D" & "E") = 42;           -- # ok
+
+   if F then                                             -- # if
+       Dummy := F + T;                                   -- # ko
+       Dummy := "+" (T, F);                              -- # ko
+       Dummy := F + T + F + T;                           -- # A_ko
+       Dummy := "+" (T, "+" (T, F));                     -- # B_ko
+
+       Dummy := One < Two;                               -- # ko
+       Dummy := Reversed."<" (One, Two);                 -- # ko
+
+       Dummy := +One = -1;                               -- # ko
+       Dummy := Reversed.Inner."+" (One) = -1;           -- # ko
+
+       Dummy := (40 & "Two") = 42;                       -- # ko
+       Dummy := (1.3 & 3.2 & 2.1) = 42.0;                -- # C_ko
+
+       Dummy := F + ("&" ("A", 8) = 42);                 -- # D_ko
+       Dummy := "+" (F, ("&" ("A", 8) = 42));            -- # E_ko
+
+       Dummy := ("A" & "B" & 42 & "D" & "E") = 42;       -- # F_ko
+   end if;
 end Op;
--- testsuite/tests/287-function_call_cov/user_operator/src/test_op.adb
+++ testsuite/tests/287-function_call_cov/user_operator/src/test_op.adb
@@ -1,16 +1,21 @@
 with Op;
 
---  LIMITATION
---  For now, gnatcov should not instrument calls to user-defined operators.
---  Check that this is true and that the proper warning is emitted.
-
 procedure Test_Op is
 begin
     Op;
 end Test_Op;
 
 --# op.adb
--- /fun/      l+ ## 0
--- /t_decl/   l+ ## 0
--- /op_call/  l? ## c?
--- /put_line/ l+ ## 0
+-- /decl/  l+ ## 0
+-- /fun/   l+ ## 0
+-- /ok/    l+ ## 0
+-- /ko/    l- ## s=>s-,f=>s-,c-
+-- /A_ko/  l- ## s=>s-,f=>s-,c-,c-,c-
+-- /B_ko/  l- ## s=>s-,f=>s-,c-,c-
+-- /C_ko/  l- ## s=>s-,f=>s-,c-,c-
+-- /D_ko/  l- ## s=>s-,f=>s-,c-,c-
+-- /E_ko/  l- ## s=>s-,f=>s-,c-,c-
+-- /F_ko/  l- ## s=>s-,f=>s-,c-,c-,c-
+-- /if/    l! ## dT-
+--# reversed.ads
+-- /ok/    l+ ## 0
