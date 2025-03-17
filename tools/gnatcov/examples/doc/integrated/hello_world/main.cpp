8f5a832e83a95cfa59a9105d8d2ec610f7c1be5d
Léo Creuse <creuse@adacore.com>
2025-03-17 15:43:39 +0100

    Doc: Add sources for integrated instrumentation examples

---
+++ tools/gnatcov/examples/doc/integrated/hello_world/main.cpp
@@ -0,0 +1,13 @@
+/******************************************************
+ *                    GNATcoverage                     *
+ *           Copyright (C) 2012-2025, AdaCore          *
+ ******************************************************/
+
+#include <iostream>
+
+int
+main (int argc, char **argv)
+{
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
