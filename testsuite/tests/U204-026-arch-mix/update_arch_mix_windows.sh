8600c49efd20b0c4e7ea8fe553005a18385baf31
Pierre-Marie de Rodat <derodat@adacore.com>
2025-05-14 09:02:00 +0000

    U204-026-arch-mix: make update scripts re-runnable
    Avoid implicit assumptions that these scripts run on "fresh state" (no
    IOD instance started, no directory copies already made, ...) so that one
    can re-run the update script after a previous (failed) attempt.

--- testsuite/tests/U204-026-arch-mix/update_arch_mix_windows.sh
+++ testsuite/tests/U204-026-arch-mix/update_arch_mix_windows.sh
@@ -9,8 +9,14 @@ fi
 
 # Create a sandbox
 cd /Users/itmgr
-git clone git-adacore:eng/cov/gnatcoverage
-anod init wave
+if ! [ -d gnatcoverage ]
+then
+    git clone git-adacore:eng/cov/gnatcoverage
+fi
+if ! [ -d wave ]
+then
+    anod init wave
+fi
 
 # Checkout the right gnatcov branch
 cd /Users/itmgr/gnatcoverage
@@ -19,19 +25,22 @@ git checkout origin/$1
 
 # Build gnatcov
 cd /Users/itmgr/wave
+anod vcs --reset
 anod vcs --add-repo gnatcoverage /Users/itmgr/gnatcoverage
-anod build gnatcov --interactive never
+anod build gnatcov --smart
 anod install gnatall -Qstable
 eval $(anod printenv gnatcov)
 eval $(anod printenv gnatall -Qstable)
 
 # Then, retrieve the sources of the arch-mix test
-rm -rf /Users/itmgr/gnatcoverage/testsuite/tests/U204-026-arch-mix/gen/x86_64-windows
-cp -r /Users/itmgr/gnatcoverage/testsuite/tests/U204-026-arch-mix /cygdrive/c/tmp
-cd /cygdrive/c/tmp/U204-026-arch-mix
+SRC_DIR=/Users/itmgr/gnatcoverage/testsuite/tests/U204-026-arch-mix
+BUILD_DIR=/cygdrive/c/tmp/U204-026-arch-mix
+rm -rf "$SRC_DIR/gen/x86_64-windows"
+rsync -ar "$SRC_DIR/" "$BUILD_DIR"
+cd "$BUILD_DIR"
 chmod +x gen.sh
 gnatcov setup
 ./gen.sh windows
 
 # Copy back the generated sources
-cp -r /cygdrive/c/tmp/U204-026-arch-mix/gen/* /Users/itmgr/gnatcoverage/testsuite/tests/U204-026-arch-mix/gen/
+rsync -ar "$BUILD_DIR/gen/" "$SRC_DIR/gen"
