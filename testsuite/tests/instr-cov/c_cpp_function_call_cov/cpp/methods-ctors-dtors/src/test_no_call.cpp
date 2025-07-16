af3dcab705c764520fab8cb5d66bce819cb52327
Matthieu Eyraud <eyraud@adacore.com>
2025-07-16 11:28:46 +0200

    Fix coverage expectations for constexpr constructors
    As constexpr are now marked as no code.

--- testsuite/tests/instr-cov/c_cpp_function_call_cov/cpp/methods-ctors-dtors/src/test_no_call.cpp
+++ testsuite/tests/instr-cov/c_cpp_function_call_cov/cpp/methods-ctors-dtors/src/test_no_call.cpp
@@ -9,7 +9,7 @@ main (void)
 //# pkg.cpp
 // /.*def_1/          l- ## f-
 // /.*def_2/          l- ## 0
-// /default_ctor_def/ l? ## f?
+// /default_ctor_def/ l. ## 0
 // /var_ctor_def/     l- ## f-
 // /return/           l- ## s-
 // /point_.*/         l- ## s-
