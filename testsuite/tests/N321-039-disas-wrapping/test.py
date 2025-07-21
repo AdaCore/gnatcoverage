1a4790f5dc72854ceb79d8974db8b120da1b97a4
Pierre-Marie de Rodat <derodat@adacore.com>
2025-07-21 13:01:23 +0000

    N321-039-disas-wrapping: rework for arm-elf compatibility

--- testsuite/tests/N321-039-disas-wrapping/test.py
+++ testsuite/tests/N321-039-disas-wrapping/test.py
@@ -3,32 +3,33 @@ Check that the 'dump-cfg' and 'disassemble-insn-properties' subcommands handle
 well instructions at the end of the address space.
 """
 
+from e3.os.process import Run
+
 from SUITE.context import thistest
-from SUITE.cutils import Wdir
-from SUITE.tutils import gprfor, gprbuild, xcov
+from SUITE.cutils import Wdir, contents_of
+from SUITE.tutils import xcov
 
 tmp_ = Wdir("tmp_")
 
-# We want to link the code at the end of the address space, so get the correct
-# upper address depending on the architecture.
+# We want to link the code (4 bytes) at the end of the address space, so get
+# the correct upper address depending on the architecture.
 
-address = 2**32 - 1 if "32bits" in thistest.options.tags else 2**64 - 1
+address = (2**32 if "32bits" in thistest.options.tags else 2**64) - 4
 
-gprfile = gprfor(
-    ["foo.s"],
-    srcdirs="..",
-    langs=("Asm"),
-    extra="""
-    package Linker is
-        for Default_Switches ("Asm") use ("-nostdlib", "-Ttext={}");
-    end Linker;
-""".format(
-        address
-    ),
-)
 exefile = "foo"
-
-gprbuild(gprfile)
+log_file = "gcc.log"
+p = Run(
+    [
+        "arm-eabi-gcc",
+        "-o",
+        exefile,
+        "../foo.s",
+        "-nostdlib",
+        f"-Ttext={hex(address)}",
+    ],
+    output=log_file,
+)
+thistest.fail_if(p.status != 0, "gcc failed:\n" + contents_of(log_file))
 
 for subcmd in ("dump-cfg", "disassemble-insn-properties"):
     # As long as GNATcov do not crash/hangs, everything is fine!
