85cb564c754e744c88b4e98e4de6ddad5a5efd23
Pierre-Marie de Rodat <derodat@adacore.com>
2025-07-21 13:26:27 +0000

    OA05-051-sharedobjs*: remove obsolete tests
    These synthetic tests were added to check that `gnatcov coverage`
    supported correctly new (binary) trace entry kinds designed to support
    shared objects correctly. No producer of binary trace files ever had
    support to generate them, and likely none ever will (shared objects are
    a native concept, and native targets are not supported anymore with
    binary traces; support for binary traces will likely soon be removed,
    too). Remove these tests.

--- testsuite/tests/OA05-051-sharedobjs/src-mylib2/mylib2.adb
+++
@@ -1,8 +0,0 @@
-package body Mylib2 is
-
-   function F (I : Integer) return Integer is
-   begin
-      return ((I / 3) + 1) * 2;
-   end F;
-
-end Mylib2;
--- testsuite/tests/OA05-051-sharedobjs/src-mylib2/mylib2.ads
+++
@@ -1,5 +0,0 @@
-package Mylib2 is
-
-   function F (I : Integer) return Integer;
-
-end Mylib2;
