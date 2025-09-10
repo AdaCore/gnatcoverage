2ff2a2a719e3c52f59418d196000f10f3f154716
Léo Creuse <creuse@adacore.com>
2025-09-10 16:46:20 +0200

    Update U204-026-arch-mix artifacts following revert of !938

--- testsuite/tests/U204-026-arch-mix/gen/arm-elf-linux/main_1.adb
+++ testsuite/tests/U204-026-arch-mix/gen/arm-elf-linux/main_1.adb
@@ -1,6 +1,6 @@
 pragma Style_Checks (Off); pragma Warnings (Off);with Pkg;
 
-with GNATcov_RTS.Buffers;with GCVRT.Pz794ac68b;with GCVRT.DB_z794ac68b;with GNATcov_RTS;with GNATcov_RTS.Buffers;pragma Compile_Time_Error(GNATcov_RTS.Version/=8,"Incompatible GNATcov_RTS version, please use the GNATcov_RTS project provided with your GNATcoverage distribution.");procedure Main_1 is
+with GNATcov_RTS.Buffers;with GCVRT.Pz794ac68b;with GCVRT.DB_z794ac68b;with GNATcov_RTS;with GNATcov_RTS.Buffers;pragma Compile_Time_Error(GNATcov_RTS.Version/=9,"Incompatible GNATcov_RTS version, please use the GNATcov_RTS project provided with your GNATcoverage distribution.");procedure Main_1 is
 begin
    GNATcov_RTS.Buffers.Witness(GCVRT.Pz794ac68b.Buffers_1.Statement_Buffer,0);Pkg.Compute (True, False);
 GCVRT.DB_z794ac68b.Dump_Buffers;end Main_1;
