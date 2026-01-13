fd733c0d06988abf55189cedab06d1f5e12ed344
Pierre-Marie de Rodat <derodat@adacore.com>
2026-01-13 13:10:25 +0000

    Ada 2022: add support for contracts in generic formal subprograms
    (cherry picked from commit e70d901e622c4e4b19a0458c2526a9b4e085703c)

---
+++ testsuite/tests/Ada2022/subp_pre_post/src/test_pkg_0.adb
@@ -0,0 +1,18 @@
+with Pkg;
+
+procedure Test_Pkg_0 is
+begin
+   null;
+end Test_Pkg_0;
+
+--# pkg.ads
+--
+--  /pre/  l- ## a-
+--  /post/ l- ## a-
+--
+--# pkg.adb
+--
+--  /div_2/ l- ## s-
+--  /stmt/  l- ## s-
+--  /check/ l- ## s-
+--  /raise/ l- ## s-
