b02afa6c0f505d7ce4dc59e41f17914b7da27381
Pierre-Marie de Rodat <derodat@adacore.com>
2026-01-26 10:29:23 +0000

    Check support for deep delta aggregates
    (cherry picked from commit 652de8613cfe179f8f7dd7061d0fb307f7e00b8e)

---
+++ testsuite/tests/gnatX/deep_delta_aggregates/src/test_0.adb
@@ -0,0 +1,13 @@
+with Pkg; use Pkg;
+
+procedure Test_0 is
+begin
+   S := (others => (0, 0));
+end Test_0;
+
+--# pkg.adb
+--
+-- /stmt/  l- ## s-
+-- /src/   l- ## 0
+-- /index/ l- ## 0
+-- /value/ l- ## 0
