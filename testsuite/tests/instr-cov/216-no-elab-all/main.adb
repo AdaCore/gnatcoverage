35f21033a5b70de08a5e60a1d045ed93fb918d21
Pierre-Marie de Rodat <derodat@adacore.com>
2026-02-13 21:29:09 +0000

    Instrument: fix No_Elaboration_Code_All detection for subprogram units
    `No_Elaboration_Code_All` pragmas can appear for subprogram units, but
    so far detection logic was run in package decl-only handling code. Move
    it to the code that handles compilation unit instead so that it covers
    all kinds of units.

--- testsuite/tests/instr-cov/216-no-elab-all/main.adb
+++ testsuite/tests/instr-cov/216-no-elab-all/main.adb
@@ -1,9 +1,15 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Gen_Proc;
 with Pkg;         use Pkg;
+with Proc;
 
 procedure Main is
    Two_Digits : constant Int_Range := (10, 99);
+
+   procedure Proc_Inst is new Gen_Proc;
 begin
+   Proc;
+   Proc_Inst;
    if In_Range (9, Two_Digits) then
       Put_Line ("ERROR!");
    elsif not In_Range (10, Two_Digits) then
