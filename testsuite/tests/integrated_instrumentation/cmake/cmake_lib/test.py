321b2e98369c96b1ac4620f13cd4d7ca569aaefb
Matthieu Eyraud <eyraud@adacore.com>
2026-04-01 14:54:53 +0200

    refactor cmake tests to make them functional under windows

--- testsuite/tests/integrated_instrumentation/cmake/cmake_lib/test.py
+++ testsuite/tests/integrated_instrumentation/cmake/cmake_lib/test.py
@@ -5,52 +5,19 @@ This is what is done by CMake when defining a library, then using it in an
 executable in the same CMakeLists.txt.
 """
 
-import os
-import os.path
-
-from SUITE.control import env
 from SUITE.cutils import Wdir
 from SCOV.minicheck import check_xcov_reports
-from SUITE.tutils import cmdrun, srctracename_for, thistest, xcov
+from SUITE.integrated_instr_utils import (
+    build_run_and_coverage,
+    CMake,
+)
+from SUITE.tutils import thistest
 
 Wdir("tmp_")
 
-cwd = os.getcwd()
-
-# Setup the instrumentation process
-xcov(
-    [
-        "setup-integration",
-        f"--files={os.path.join(cwd, '..', 'src', 'main.c')}",
-        f"--files={os.path.join(cwd, '..', 'src', 'lib.c')}",
-        "--compilers=gcc",
-        "--level=stmt",
-    ]
-)
-
-# Shadow the compiler driver with the generated wrapper
-env.add_search_path(env_var="PATH", path=cwd)
-
-# Then, run the build process unchanged
-compiler_wrapper = os.path.join(cwd, "gcc")
-cmdrun(
-    ["cmake", "..", f"-DCMAKE_C_COMPILER={compiler_wrapper}"], for_pgm=False
-)
-cmdrun(["make"], for_pgm=False)
-
-# Run the executable
-cmdrun(["hello_world"], for_pgm=True)
-
-# Check coverage expectations
-xcov(
-    [
-        "coverage",
-        "--level=stmt",
-        "--sid=main.c.sid",
-        "--sid=lib.c.sid",
-        "-axcov",
-        srctracename_for("main"),
-    ]
+wfs = [CMake()]
+build_run_and_coverage(
+    wfs=wfs, files_of_interest=["../src/main.c", "../src/lib.c"]
 )
 check_xcov_reports(
     ".",
