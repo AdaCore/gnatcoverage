f3fb5cd2c907d92ab5c03fd9a389d55befa9a985
Matthieu Eyraud <eyraud@adacore.com>
2026-08-21 15:13:15 +0200

    End the statement block at the end of an extended return
    And do not create a new statement block without ending the one enclosing
    the extended return statement.

---
+++ testsuite/tests/instr_cov/block_control_flow/extended_return/pkg.adb
@@ -0,0 +1,12 @@
+package body Pkg is
+
+   function Ext_Return (X : Integer) return Integer is
+      T : Integer := X;
+   begin
+      return R : Integer := T do
+         R := R + 1;
+      end return;
+      T := 0;
+   end Ext_Return;
+
+end Pkg;
