2892674f196043530b27e0d5516d500c69bb5657
Pierre-Marie de Rodat <derodat@adacore.com>
2026-08-25 13:31:09 +0000

    Update and enable t_207_srctrace_version and t_U204_026_arch_mix

--- testsuite/tests/t_U204_026_arch_mix/gen/x86_64-windows/gcvrt-bz5596b68c.ads
+++ testsuite/tests/t_U204_026_arch_mix/gen/x86_64-windows/gcvrt-bz5596b68c.ads
@@ -4,7 +4,7 @@ with System;
 with GNATcov_RTS.Buffers;       use GNATcov_RTS.Buffers;
 with GNATcov_RTS.Buffers.Lists; use GNATcov_RTS.Buffers.Lists;
 
-package GCVRT.Bz794ac68b is
+package GCVRT.Bz5596b68c is
    pragma Preelaborate;
 
    package Buffers_1 is
@@ -13,27 +13,27 @@ package GCVRT.Bz794ac68b is
       Statement_Buffer_Address : constant System.Address :=
          Statement_Buffer'Address;
       pragma Export
-        (C, Statement_Buffer_Address, "xcov__buf_stmt__zed93bb71_1");
+        (C, Statement_Buffer_Address, "xcov__buf_stmt__z31801a72_1");
 
       Decision_Buffer         : Coverage_Buffer_Type (0 .. -1) :=
         (others => False);
       Decision_Buffer_Address : constant System.Address :=
          Decision_Buffer'Address;
       pragma Export
-        (C, Decision_Buffer_Address, "xcov__buf_dc__zed93bb71_1");
+        (C, Decision_Buffer_Address, "xcov__buf_dc__z31801a72_1");
 
       MCDC_Buffer : Coverage_Buffer_Type (0 .. -1) :=
         (others => False);
       MCDC_Buffer_Address : constant System.Address := MCDC_Buffer'Address;
       pragma Export
-        (C, MCDC_Buffer_Address, "xcov__buf_mcdc__zed93bb71_1");
+        (C, MCDC_Buffer_Address, "xcov__buf_mcdc__z31801a72_1");
 
-      Filename : constant String := "C:\tmp\t_U204_026_arch_mix\main_1.adb";
+      Filename : constant String := "C:\tmp\t_U204_026_arch_mix\main_2.adb";
 
       Buffers : aliased constant GNATcov_RTS_Coverage_Buffers :=
-        (Fingerprint             => (106, 13, 66, 61, 138, 186, 69, 82, 48, 55, 131, 49, 211, 239, 150, 134, 208, 146, 21, 201),
+        (Fingerprint             => (91, 111, 84, 47, 59, 170, 241, 201, 183, 106, 198, 225, 186, 129, 94, 99, 218, 251, 32, 207),
          Filename                => (Filename'Address, Filename'Length),
-         Bit_Maps_Fingerprint    => (228, 252, 99, 232, 172, 243, 155, 56, 89, 239, 143, 211, 57, 206, 9, 239, 178, 154, 99, 251),
+         Bit_Maps_Fingerprint    => (58, 157, 18, 8, 23, 166, 32, 170, 215, 100, 28, 213, 238, 91, 76, 213, 203, 70, 164, 128),
          Annotations_Fingerprint => (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
          Statement               => Statement_Buffer'Address,
          Decision                => Decision_Buffer'Address,
@@ -49,6 +49,6 @@ package GCVRT.Bz794ac68b is
      );
    C_Buffers_Group : aliased constant GNATcov_RTS_Coverage_Buffers_Group :=
       ( 1, Buffers_Group'Address);
-   pragma Export (C, C_Buffers_Group, "gnatcov_rts_buffers_z794ac68b_buffers");
+   pragma Export (C, C_Buffers_Group, "gnatcov_rts_buffers_z5596b68c_buffers");
 
-end GCVRT.Bz794ac68b;
+end GCVRT.Bz5596b68c;
--- testsuite/tests/t_U204_026_arch_mix/gen/x86_64-windows/gcvrt-pz5596b68c.ads
+++ testsuite/tests/t_U204_026_arch_mix/gen/x86_64-windows/gcvrt-pz5596b68c.ads
@@ -5,19 +5,19 @@ with GNATcov_RTS;
 with GNATcov_RTS.Buffers;
 pragma Compile_Time_Error (GNATcov_RTS.Version /= 12 ,"Incompatible GNATcov_RTS version, please use the GNATcov_RTS project provided with your GNATcoverage distribution.");
 
-package GCVRT.Pz794ac68b is
+package GCVRT.Pz5596b68c is
    pragma Pure;
 
    package Buffers_1 is
       Statement_Buffer : constant System.Address;
-      pragma Import (C, Statement_Buffer, "xcov__buf_stmt__zed93bb71_1");
+      pragma Import (C, Statement_Buffer, "xcov__buf_stmt__z31801a72_1");
 
       Decision_Buffer : constant System.Address;
-      pragma Import (C, Decision_Buffer, "xcov__buf_dc__zed93bb71_1");
+      pragma Import (C, Decision_Buffer, "xcov__buf_dc__z31801a72_1");
 
       MCDC_Buffer : constant System.Address;
-      pragma Import (C, MCDC_Buffer, "xcov__buf_mcdc__zed93bb71_1");
+      pragma Import (C, MCDC_Buffer, "xcov__buf_mcdc__z31801a72_1");
    end Buffers_1;
 
 
-end GCVRT.Pz794ac68b;
+end GCVRT.Pz5596b68c;
